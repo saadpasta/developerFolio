67a8e05ad3ad150fcfed3f549fe3b991bc2e13fc
saadpasta <saadpasta70@gmail.com>
2019-11-19 16:00:12 +0500

    add : portfolio/ config  file

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,13 +1,89 @@
-export const portfolio = {
-  gretting: {
-    title: "Hi all I'm Saad",
-    subTitle: "A passionate Full Stack Software Developer 🚀 having an experience of building web applications with React / Node / React Native and much more.",
-    socialMediaLinks : {
-        github:"https://github.com/saadpasta",
-        linkedin : "https://www.linkedin.com/in/saadpasta/",
-        gmail : "saadpasta70@gmail.com",
-        gitlab:"https://gitlab.com/saadpasta",
-        facebook:"https://www.facebook.com/saad.pasta7"
-    }
-  }
+/* Change this file to get your personal Porfolio */
+
+const gretting = {
+  /* Your Summary And Gretting Section */
+  title: "Hi all 👋 I'm Saad",
+  subTitle:
+    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks",
+  resumeLink: "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing"
 };
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/saadpasta",
+  linkedin: "https://www.linkedin.com/in/saadpasta/",
+  gmail: "saadpasta70@gmail.com",
+  gitlab: "https://gitlab.com/saadpasta",
+  facebook: "https://www.facebook.com/saad.pasta7"
+};
+
+const skillsSection = {
+  /* Your Skills Section  */
+  title: "What i do",
+  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANT TO EXPLORE EVERY TECH STACK",
+  skills: [
+    "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications",
+    "⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks",
+    "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+  ],
+
+  /* Make Sure You include correct Font Awesome Classname to view your icon */
+  /* https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt"
+    },
+    {
+      skillName: "sass",
+      fontAwesomeClassname: "fab fa-sass"
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js"
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "fab fa-react"
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "fab fa-node"
+    },
+    {
+      skillName: "swift",
+      fontAwesomeClassname: "fab fa-swift"
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "fab fa-npm"
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "aws",
+      fontAwesomeClassname: "fab fa-aws"
+    },
+    {
+      skillName: "firebase",
+      fontAwesomeClassname: "fas fa-fire"
+    },
+    {
+      skillName: "python",
+      fontAwesomeClassname: "fab fa-python"
+    },
+    {
+      skillName: "docker",
+      fontAwesomeClassname: "fab fa-docker"
+    }
+  ]
+};
+
+export { gretting, socialMediaLinks, skillsSection };
