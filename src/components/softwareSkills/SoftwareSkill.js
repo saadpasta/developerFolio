4df50dac4142237e0ef50ae980c87224803bed35
saadpasta <saadpasta70@gmail.com>
2019-11-19 16:00:48 +0500

    modified : Software Skills with new config File

--- src/components/softwareSkills/SoftwareSkill.js
+++ src/components/softwareSkills/SoftwareSkill.js
@@ -1,50 +1,19 @@
 import React from "react";
 import "./SoftwareSkill.css";
+import { skillsSection } from "../../portfolio";
 
 export default function SoftwareSkill() {
   return (
     <div>
       <div className="software-skills-main-div">
         <ul className="dev-icons">
-          <li className="software-skill-inline" name="html-5">
-            <i className="fab fa-html5"></i>
-          </li>
-          <li className="software-skill-inline" name="css3">
-            <i className="fab fa-css3-alt"></i>
-          </li>
-          <li className="software-skill-inline" name="sass">
-            <i className="fab fa-sass"></i>
-          </li>
-          <li className="software-skill-inline" name="JavaScript">
-            <i className="fab fa-js"></i>
-          </li>
-          <li className="software-skill-inline" name="reactjs">
-            <i className="fab fa-react"></i>
-          </li>
-          <li className="software-skill-inline" name="nodejs">
-            <i className="fab fa-node"></i>
-          </li>
-          <li className="software-skill-inline" name="swift">
-            <i className="fab fa-swift"></i>
-          </li>
-          <li className="software-skill-inline" name="npm">
-            <i className="fab fa-npm"></i>
-          </li>
-          <li className="software-skill-inline" name="sql-database">
-            <i className="fas fa-database"></i>
-          </li>
-          <li className="software-skill-inline" name="aws">
-            <i className="fab fa-aws"></i>
-          </li>
-          <li className="software-skill-inline" name="firebase">
-            <i className="fas fa-fire"></i>
-          </li>
-          <li className="software-skill-inline" name="python">
-            <i className="fab fa-python"></i>
-          </li>
-          <li className="software-skill-inline" name="docker">
-            <i className="fab fa-docker"></i>
-          </li>
+          {skillsSection.softwareSkills.map(skills => {
+            return (
+              <li className="software-skill-inline" name={skills.skillName}>
+                <i className={skills.fontAwesomeClassname}></i>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
