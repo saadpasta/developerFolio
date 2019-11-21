d88befbd0d382f9597ed3f49ef8426431c3845d6
saadpasta <saadpasta70@gmail.com>
2019-11-21 17:15:59 +0500

    modified : blog section with config

--- src/containers/blogs/Blogs.js
+++ src/containers/blogs/Blogs.js
@@ -1,32 +1,29 @@
 import React from "react";
 import "./Blog.css";
 import BlogCard from "../../components/blogCard/BlogCard";
+import { blogSection } from "../../portfolio";
 
 export default function Blogs() {
   return (
     <div className="main" id="blogs">
       <div className="blog-header">
-        <h1 className="blog-header-text">Blogs</h1>
-        <p className="subTitle blog-subtitle">With Love for Developing cool stuff. i love to write and teach others what i have learned</p>
+        <h1 className="blog-header-text">{blogSection.title}</h1>
+        <p className="subTitle blog-subtitle">{blogSection.subtitle}</p>
       </div>
       <div className="blog-main-div">
         <div className="blog-text-div">
-          <BlogCard
-            blog={{
-              url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
-              image: "https://miro.medium.com/max/400/1*pYHONrA_3QM4dgtBSKrYWQ.jpeg",
-              title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
-              description: "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min."
-            }}
-          />
-          <BlogCard
-            blog={{
-              url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
-              image: "https://miro.medium.com/max/1280/1*o08b2PW-1PVs94riZuhNMA.png",
-              title: "Why REACT Is The Best?",
-              description: "React is a JavaScript library for building the user interface. It is maintained by Facebook and a community of individual developers and companies."
-            }}
-          />
+          {blogSection.blogs.map(blog => {
+            return (
+              <BlogCard
+                blog={{
+                  url: blog.url,
+                  image: blog.image,
+                  title: blog.title,
+                  description: blog.description
+                }}
+              />
+            );
+          })}
         </div>
       </div>
     </div>
--- src/portfolio.js
+++ src/portfolio.js
@@ -109,4 +109,26 @@ const bigProjects = {
   ]
 };
 
-export { gretting, socialMediaLinks, skillsSection, openSource, bigProjects };
+const blogSection = {
+  /* Blogs Section */
+
+  title: "Blogs",
+  subtitle: "With Love for Developing cool stuff. i love to write and teach others what i have learned",
+
+  blogs: [
+    {
+      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
+      image: "https://miro.medium.com/max/400/1*pYHONrA_3QM4dgtBSKrYWQ.jpeg",
+      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      description: "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min."
+    },
+    {
+      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
+      image: "https://miro.medium.com/max/1280/1*o08b2PW-1PVs94riZuhNMA.png",
+      title: "Why REACT Is The Best?",
+      description: "React is a JavaScript library for building the user interface. It is maintained by Facebook and a community of individual developers and companies."
+    }
+  ]
+};
+
+export { gretting, socialMediaLinks, skillsSection, openSource, bigProjects, blogSection };
