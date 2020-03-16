6ece55930cac82c7a1ede26b3e7e049956d86657
Naveen M K <naveen521kk@gmail.com>
2020-03-16 14:06:51 +0530

    Removed unnecessary functions

--- src/containers/topbutton/Top.js
+++ src/containers/topbutton/Top.js
@@ -20,9 +20,6 @@ export default function Top() {
     }
         
     // When the user clicks on the button, scroll to the top of the document
-    function goTop() {
-        alert('Hello!');
-      }
     return (
         <button onClick={TopEvent} id="topButton" title="Go to top"><i class="fa fa-hand-o-up" aria-hidden="true"></i></button>
         );
