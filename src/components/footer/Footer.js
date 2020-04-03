42f29983372d868c255ee18fe1b28edfaf2d1ac9
Ashutosh Hathidara <ashutoshhathidara98@gmail.com>
2020-04-03 15:24:53 +0530

    Added Basic Reveal Fade animations

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,7 @@ import { Fade } from "react-reveal";
 
 export default function Footer() {
   return (
-  	<Fade bottom duration={1000} distance="5px">
+    <Fade bottom duration={1000} distance="5px">
     <div className="footer-div">
       <p className="footer-text">Made with ❤️ by Saad Pasta</p>
     </div>
