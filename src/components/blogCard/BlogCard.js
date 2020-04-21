d8544f7ff06778d412c46e7bcc96f5514167ba89
Saad Pasta <saadpasta70@gmail.com>
2020-04-21 10:44:19 +0500

    Redesign Blog Cards

--- src/components/blogCard/BlogCard.js
+++ src/components/blogCard/BlogCard.js
@@ -4,18 +4,14 @@ import "./BlogCard.css";
 export default function BlogCard({ blog }) {
   return (
     <div>
-      <div class="container">
-        <div class="square">
-          <img src={blog.image} alt="blog" className="blog-card-image mask" />
-          <div class="blog-card-title">{blog.title}</div>
-          <p className="blog-card-subtitle">{blog.description}</p>
-
-          <div>
-            <a href={blog.url} target="_" class="button">
-              Read More
-            </a>
+      <div class="blog-container">
+        <a class="blog-card" href="#blog" >
+          <h3 className="blog-title">{blog.title}</h3>
+          <p class="small">{blog.description}</p>
+          <div class="go-corner" >
+            <div class="go-arrow">→</div>
           </div>
-        </div>
+        </a>
       </div>
     </div>
   );
