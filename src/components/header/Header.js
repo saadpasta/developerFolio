6d3a3c18c6046bb5e4220def6a1104643ae40868
MohammadHasham <mhasham@sparkcognition.com>
2020-04-22 11:46:19 +0500

    Added Dark Mode Feature

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./Header.css";
-import {Fade} from "react-reveal";
+import { Fade } from "react-reveal";
+import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 
-function Header() {
+function Header({ style }) {
   return (
     <Fade top duration={1000} distance="20px">
     <div>
@@ -16,29 +17,30 @@ function Header() {
         <label className="menu-icon" htmlFor="menu-btn">
           <span className="navicon"></span>
         </label>
-        <ul className="menu">
+        <ul className="menu" style={{ ...style }}>
           <li>
-            <a href="#skills">Skills</a>
+            <a href="#skills" style={{ ...style }}>Skills</a>
           </li>
           <li>
-            <a href="#projects">Projects</a>
+            <a href="#projects" style={{ ...style }}>Projects</a>
           </li>
           <li>
-            <a href="#opensource">Open Source</a>
+            <a href="#opensource" style={{ ...style }}>Open Source</a>
           </li>
           <li>
-            <a href="#blogs">Blogs</a>
+            <a href="#blogs" style={{ ...style }}>Blogs</a>
           </li>
           <li>
-            <a href="#talks">Talks</a>
+            <a href="#talks" style={{ ...style }}>Talks</a>
           </li>
           <li>
-            <a href="#achievements">Achievements</a>
+            <a href="#achievements" style={{ ...style }} >Achievements</a>
           </li>
           <li>
-            <a href="#contact">Contact Me</a>
+            <a href="#contact" style={{ ...style }}>Contact Me</a>
           </li>
         </ul>
+        <ToggleSwitch />
       </header>
     </div>
     </Fade>
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -13,12 +13,27 @@ import Talks from "./talks/Talks";
 import Podcast from "./podcast/Podcast";
 import Top from "./topbutton/Top";
 import Twitter from "./twitter-embed/twitter";
+import { StyleProvider } from '../contexts/StyleContext';
 
 export default class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDark: false,
+      style: { backgroundColor: 'none', color: 'black' }
+    }
+  }
+
+  changeTheme = () => {
+    this.setState({ style: { backgroundColor: 'black', color: 'white', transition: '0.1s' }, isDark: !this.state.isDark })
+  }
+
   render() {
     return (
-      <div>
-        <Header />
+      <div style={this.state.isDark ? this.state.style : null} >
+        <StyleProvider value={{ changeTheme: this.changeTheme }} >
+          <Header style={this.state.isDark ? this.state.style : null} />
+        </StyleProvider>
         <Greeting />
         <Skills />
         <StackProgress />
