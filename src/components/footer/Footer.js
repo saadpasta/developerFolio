cae0d70ab909a06c838366237e430e68217a184d
Ahmad Awais <me@AhmadAwais.com>
2020-04-30 18:37:07 +0500

    📦 NEW: Format

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Footer.css';
 import {Fade} from 'react-reveal';
-import emoji from 'react-easy-emoji';
 
 const Footer = () => (
 	<Fade bottom duration={1000} distance="5px">
