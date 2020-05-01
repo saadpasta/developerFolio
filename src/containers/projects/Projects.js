6441339108a21d50a7b524a4ed3b16e0febfafcf
MohammadHasham <mhasham@sparkcognition.com>
2020-05-01 14:31:21 +0500

    removed console.log

--- src/containers/projects/Projects.js
+++ src/containers/projects/Projects.js
@@ -3,12 +3,14 @@ import ApolloClient from "apollo-boost";
 import { gql } from "apollo-boost";
 import "./Project.css";
 import Button from "../../components/button/Button";
-import { openSource,socialMediaLinks  } from "../../portfolio";
+import { openSource, socialMediaLinks } from "../../portfolio";
 import { Fade } from "react-reveal";
-import { StyleConsumer } from "../../contexts/StyleContext"
-import Loading from '../../containers/loading/Loading'
+import { StyleConsumer } from "../../contexts/StyleContext";
+import Loading from "../../containers/loading/Loading";
 export default function Projects() {
-  const GithubRepoCard = lazy(() => import('../../components/githubRepoCard/GithubRepoCard'));
+  const GithubRepoCard = lazy(() =>
+    import("../../components/githubRepoCard/GithubRepoCard")
+  );
   const FailedLoading = () => null;
   const renderLoader = () => <Loading />;
   const [repo, setrepo] = useState([]);
@@ -17,7 +19,6 @@ export default function Projects() {
     getRepoData();
   }, []);
 
-  
   function getRepoData() {
     const client = new ApolloClient({
       uri: "https://api.github.com/graphql",
@@ -62,34 +63,35 @@ export default function Projects() {
         `,
       })
       .then((result) => {
-        setrepoFunction(result.data.user.pinnedItems.edges);
-        console.log(result);
-      })
-      .catch(function (error) {
-        console.log(error);
-        setrepoFunction("Error");
-        console.log("Because of this Error, nothing is shown in place of Projects section. Projects section not configured");
+        setrepoFunction(result.data.repositoryOwner.pinnedRepositories.edges);
       });
   }
 
   function setrepoFunction(array) {
     setrepo(array);
   }
-  if (!(typeof repo === 'string' || repo instanceof String)){
+  if (!(typeof repo === "string" || repo instanceof String)) {
     return (
       <Suspense fallback={renderLoader()}>
         <div className="main" id="opensource">
           <h1 className="project-title">Open Source Projects</h1>
           <div className="repo-cards-div-main">
             {repo.map((v, i) => {
-              return <GithubRepoCard repo={v} key={v.node.id} isDark={isDark}/>;
+              return (
+                <GithubRepoCard repo={v} key={v.node.id} isDark={isDark} />
+              );
             })}
           </div>
-          <Button text={"More Projects"} className="project-button" href={socialMediaLinks.github} newTab={true} />
+          <Button
+            text={"More Projects"}
+            className="project-button"
+            href={socialMediaLinks.github}
+            newTab={true}
+          />
         </div>
       </Suspense>
     );
-  } else{
-      return(<FailedLoading />);
-    }
+  } else {
+    return <FailedLoading />;
   }
+}
