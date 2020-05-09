6eed319c209a9ef938844c6cc89bc8653871549e
MohammadHasham <mhasham@sparkcognition.com>
2020-05-09 19:32:56 +0500

    added minor css style fixes

--- src/containers/achievement/Achievement.js
+++ src/containers/achievement/Achievement.js
@@ -12,43 +12,26 @@ export default function Achievement() {
   }
   return (
     <Fade bottom duration={1000} distance="20px">
-      <div className="main" id="achievements">
-        <div className="achievement-main-div">
-          <div className="achievement-header">
-            <h1
-              className={
-                isDark
-                  ? "dark-mode heading achievement-heading"
-                  : "heading achievement-heading"
-              }
-            >
-              {achievementSection.title}
-            </h1>
-            <p
-              className={
-                isDark
-                  ? "dark-mode subTitle achievement-subtitle"
-                  : "subTitle achievement-subtitle"
-              }
-            >
-              {achievementSection.subtitle}
-            </p>
-          </div>
-          <div className="achievement-cards-div">
-            {achievementSection.achivementsCards.map((card) => {
-              return (
-                <AchivementCard
-                  isDark={isDark}
-                  cardInfo={{
-                    title: card.title,
-                    description: card.subtitle,
-                    image: card.image,
-                    footer: card.footerLink,
-                  }}
-                />
-              );
-            })}
-          </div>
+    <div className="main" id="achievements">
+      <div className="achievement-main-div">
+        <div className="achievement-header">
+          <h1 className={isDark ? "dark-mode heading achievement-heading":"heading achievement-heading"}>{achievementSection.title}</h1>
+          <p className={isDark ? "dark-mode subTitle achievement-subtitle":"subTitle achievement-subtitle"}>{achievementSection.subtitle}</p>
+        </div>
+        <div className="achievement-cards-div">
+          {achievementSection.achivementsCards.map(card => {
+            return (
+              <AchivementCard
+                isDark={isDark}
+                cardInfo={{
+                  title: card.title,
+                  description: card.subtitle,
+                  image: card.image,
+                  footer: card.footerLink
+                }}
+              />
+            );
+          })}
         </div>
       </div>
     </Fade>
--- src/containers/podcast/Podcast.js
+++ src/containers/podcast/Podcast.js
@@ -2,27 +2,40 @@ import React, { useContext } from "react";
 import "./Podcast.css";
 import { podcastSection } from "../../portfolio";
 import { Fade } from "react-reveal";
-import StyleContext from "../../contexts/StyleContext";
+import { StyleConsumer } from "../../contexts/StyleContext";
 
 export default function Podcast() {
-  const { isDark } = useContext(StyleContext);
+  const { isDark } = useContext(StyleConsumer);
   return (
     <Fade bottom duration={1000} distance="20px">
-    <div className="main">
-      <div className="podcast-header">
-        <h1 className="podcast-header-title">{podcastSection.title}</h1>
-        <p className={ isDark ? "dark-mode podcast-header-subtitle":"subTitle podcast-header-subtitle"}>{podcastSection.subtitle}</p>
+      <div className="main">
+        <div className="podcast-header">
+          <h1 className="podcast-header-title">{podcastSection.title}</h1>
+          <p
+            className={
+              isDark
+                ? "dark-mode podcast-header-subtitle"
+                : "subTitle podcast-header-subtitle"
+            }
+          >
+            {podcastSection.subtitle}
+          </p>
+        </div>
+        <div className="podcast-main-div">
+          {podcastSection.podcast.map((podcastLink) => {
+            return (
+              <div>
+                <iframe
+                  className="podcast"
+                  src={podcastLink}
+                  frameborder="0"
+                  scrolling="no"
+                ></iframe>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <div className="podcast-main-div">
-        {podcastSection.podcast.map(podcastLink => {
-          return (
-            <div>
-              <iframe className= "podcast" src={podcastLink} frameborder="0" scrolling="no"></iframe>
-            </div>
-          );
-        })}
-      </div>
-    </div>
     </Fade>
   );
 }
