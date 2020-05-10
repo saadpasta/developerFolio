58af716fa557752d2dd84a677d1b1299f8421e67
garimasingh128 <garingh128@gmail.com>
2020-05-10 22:16:58 +0530

    Solves issue #120 and some minor changes

--- src/components/socialMedia/SocialMedia.js
+++ src/components/socialMedia/SocialMedia.js
@@ -5,26 +5,47 @@ import { socialMediaLinks } from "../../portfolio";
 export default function socialMedia() {
   return (
     <div className="social-media-div">
-      <a href={socialMediaLinks.github} className="icon-button github" target="_blank">
+      
+      {socialMediaLinks.github ? 
+      (<a href={socialMediaLinks.github} className="icon-button github" target="_blank">
         <i className="fab fa-github"></i>
         <span></span>
-      </a>
+      </a>)
+      :
+      null}
+
+      {socialMediaLinks.linkedin ?
       <a href={socialMediaLinks.linkedin} className="icon-button linkedin" target="_blank">
         <i className="fab fa-linkedin-in"></i>
         <span></span>
       </a>
+      :
+      null}
+
+      {socialMediaLinks.gmail ?
       <a href={`mailto:${socialMediaLinks.gmail}`} className="icon-button google" target="_blank">
         <i className="fab fa-google"></i>
         <span></span>
       </a>
+      :
+      null}
+
+      {socialMediaLinks.gitlab ?
       <a href={socialMediaLinks.gitlab} className="icon-button gitlab" target="_blank">
         <i className="fab fa-gitlab"></i>
         <span></span>
       </a>
+      :
+      null }
+
+      {socialMediaLinks.facebook ?
       <a href={socialMediaLinks.facebook} className="icon-button facebook" target="_blank">
         <i className="fab fa-facebook-f"></i>
         <span></span>
       </a>
+      :
+      null}
+
     </div>
   );
 }
