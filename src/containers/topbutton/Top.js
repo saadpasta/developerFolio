e40207a770579da7657962546df9a90f6c4667eb
Naveen M K <naveen@syrusdark.website>
2020-05-15 14:06:01 +0530

    Remove Font-Awesome (#127)
    * Removed src\assets\font-awesome
    
    * Single to double quotes src/index.js
    
    * Update Top.js with New Icon name
    
    * Add crossorigin to link preload in index.html
    
    * Added font-display shift for replace of fonts

--- src/containers/topbutton/Top.js
+++ src/containers/topbutton/Top.js
@@ -7,7 +7,7 @@ export default function Top() {
         document.body.scrollTop = 0; // For Safari
         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     }
-    // When the user scrolls down 20px from the top of the document, show the button    
+    // When the user scrolls down 20px from the top of the document, show the button   
     function scrollFunction() {
         if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
             document.getElementById("topButton").style.visibility = "visible";
@@ -16,9 +16,9 @@ export default function Top() {
         }
     }
     window.onscroll = function() {scrollFunction();};
-    window.onload = function() {scrollFunction();}; //To make sure that this button is not visible at starting.    
+    window.onload = function() {scrollFunction();}; //To make sure that this button is not visible at starting.
     // When the user clicks on the button, scroll to the top of the document
     return (
-        <button onClick={TopEvent} id="topButton" title="Go to top"><i class="fa fa-hand-o-up" aria-hidden="true"></i></button>
+        <button onClick={TopEvent} id="topButton" title="Go to top"><i class="fas fa-hand-point-up" aria-hidden="true"></i></button>
         );
     }
