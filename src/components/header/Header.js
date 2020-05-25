0f1f741b09e208fe63cc3c947239d6ae8d590099
Pulkit <Pulkitbanta55555@gmail.com>
2020-05-25 20:06:22 +0530

    chronological order in navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -22,19 +22,19 @@ function Header() {
             <a href="#skills">Skills</a>
           </li>
           <li>
-            <a href="#projects">Projects</a>
+            <a href="#opensource">Open Source</a>
           </li>
           <li>
-            <a href="#opensource">Open Source</a>
+          <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#achievements">Achievements</a>
           </li>
           <li>
             <a href="#blogs">Blogs</a>
           </li>
           <li>
             <a href="#talks">Talks</a>
-          </li>
-          <li>
-            <a href="#achievements">Achievements</a>
           </li>
           <li>
             <a href="#contact">Contact Me</a>
