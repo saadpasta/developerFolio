c312c5f8f46e808b4eb927c9c27373fb94db0c1f
Pulkit Banta <pulkitbanta55555@gmail.com>
2020-05-25 22:12:13 +0530

    Chronological order in navbar (#142)

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./Header.css";
 import {Fade} from "react-reveal";
-import {greeting} from "../../portfolio";
+import {greeting, workExperiences} from "../../portfolio";
 
 function Header() {
+  const exp = workExperiences.viewExperiences;
   return (
     <Fade top duration={1000} distance="20px">
     <div>
@@ -21,20 +22,25 @@ function Header() {
           <li>
             <a href="#skills">Skills</a>
           </li>
+          { exp === true &&
+            <li>
+              <a href="#experience">Work Experiences</a>
+            </li>
+          }
+          <li>
+            <a href="#opensource">Open Source</a>
+          </li>
           <li>
             <a href="#projects">Projects</a>
           </li>
           <li>
-            <a href="#opensource">Open Source</a>
+            <a href="#achievements">Achievements</a>
           </li>
           <li>
             <a href="#blogs">Blogs</a>
           </li>
           <li>
             <a href="#talks">Talks</a>
-          </li>
-          <li>
-            <a href="#achievements">Achievements</a>
           </li>
           <li>
             <a href="#contact">Contact Me</a>
