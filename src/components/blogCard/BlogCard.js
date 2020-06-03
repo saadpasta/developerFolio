b4bb61fa2cb3aff45e61ed18a7c9ab2c4e090909
Akshay Kumar <email.akshay98@gmail.com>
2020-06-03 12:53:22 +0530

    Redirect to blog url on button click

--- src/components/blogCard/BlogCard.js
+++ src/components/blogCard/BlogCard.js
@@ -2,6 +2,13 @@ import React from "react";
 import "./BlogCard.css";
 
 export default function BlogCard({ blog }) {
+  function openUrlInNewTab(url) {
+    if(url !== undefined) {
+      var win = window.open(url, "_blank");
+      win.focus();
+    }
+  }
+
   return (
     <div>
       <div class="blog-container">
@@ -9,7 +16,7 @@ export default function BlogCard({ blog }) {
           <h3 className="blog-title">{blog.title}</h3>
           <p class="small">{blog.description}</p>
           <div class="go-corner" >
-            <div class="go-arrow">→</div>
+            <div class="go-arrow" onClick={() => openUrlInNewTab(blog.url)}>→</div>
           </div>
         </a>
       </div>
