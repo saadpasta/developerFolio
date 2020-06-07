7af721fa9be1517bc2d5dfc370ad92f09673c24c
sanjay051099 <66544102+sanjay051099@users.noreply.github.com>
2020-06-07 15:15:58 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,8 +7,8 @@ import emoji from "react-easy-emoji";
 
 const greeting = {
   /* Your Summary And Greeting Section */
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Sanjay",
+  title: "Hi all, I'm Sanjay",
   subTitle: emoji("A passionate Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."),
   resumeLink: "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing"
 };
