24d877343cbbefc8a16298fc569941a0962e0905
Syrus Dark <naveen@syrusdark.website>
2020-06-07 14:47:36 +0530

    Add loading screen tw,profile

--- src/components/githubProfileCard/GithubProfileCard.js
+++ src/components/githubProfileCard/GithubProfileCard.js
@@ -12,7 +12,7 @@ export default function GithubProfileCard({prof}) {
     prof.hireable="No";
   }
   return (
-    <Fade bottom duration={1000} distance="20px">
+
       <div className="main" id="contact">
         <h1 className="prof-title">Reach Out to me!</h1>
       <div className="row">
@@ -37,6 +37,6 @@ export default function GithubProfileCard({prof}) {
         </div>
       </div>
     </div>
-	</Fade>
+
     );
 }
--- src/containers/profile/Profile.js
+++ src/containers/profile/Profile.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
-import GithubProfileCard from "../../components/githubProfileCard/GithubProfileCard";
+import React, { useState, useEffect  ,lazy, Suspense } from "react";
+//import GithubProfileCard from "../../components/githubProfileCard/GithubProfileCard";
 import axios from "axios";
 import { openSource } from "../../portfolio";
 import Contact from "../contact/Contact";
 
-
+const renderLoader = () => <div className="centerContent loader"></div>;
+const GithubProfileCard = lazy(() => import('../../components/githubProfileCard/GithubProfileCard'));
 export default function Profile() {
   const [prof, setrepo] = useState([]);
   function setProfileFunction(array) {
@@ -14,13 +15,23 @@ export default function Profile() {
     const client=axios.get("https://api.github.com/users/"+openSource.githubUserName)
       .then(function(response){
             setProfileFunction(response.data);
-    });
+      })
+      .catch(function (error) {
+          console.log(error);
+          setProfileFunction(error);
+          console.log("Because of this Error Contact Section is Showed instead of Profile");
+          openSource.showGithubProfile = "false";
+        });
   }
   useEffect(() => {
     getProfileData();
   }, []);
-  if (openSource.showGithubProfile === "true"){
-    return ( <GithubProfileCard prof={prof} key={prof.id} /> );
+  if (openSource.showGithubProfile === "true" && (typeof prof === 'string' || prof instanceof String)){
+    return (
+      <Suspense fallback={renderLoader()}>
+        <GithubProfileCard prof={prof} key={prof.id} /> 
+      </Suspense>
+       );
   } else {
     return(<Contact />);
   }
