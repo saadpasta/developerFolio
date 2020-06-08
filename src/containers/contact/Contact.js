79939b4f45cab3983819df7a9359bd5c23c30b17
MohammadHasham <mhasham@sparkcognition.com>
2020-06-08 23:13:17 +0500

    added dark contact image

--- src/containers/contact/Contact.js
+++ src/containers/contact/Contact.js
@@ -23,25 +23,18 @@ export default function Contact() {
               {contactInfo.subtitle}
             </p>
 
-            <div
-              className={
-                isDark ? "dark-mode contact-text-div" : "contact-text-div"
-              }
-            >
-              <a className="contact-detail" href={"tel:" + contactInfo.number}>
-                {contactInfo.number}
-              </a>
-              <br />
-              <br />
-              <a
-                className="contact-detail-email"
-                href={"mailto:" + contactInfo.email_address}
-              >
-                {contactInfo.email_address}
-              </a>
-              <br />
-              <br />
-              <SocialMedia />
+                    <div className={isDark ? "dark-mode contact-text-div" : "contact-text-div"}>
+                        <a className="contact-detail" href={"tel:" + contactInfo.number}>{contactInfo.number}</a>
+                        <br/><br/>
+                        <a className="contact-detail-email"
+                           href={"mailto:" + contactInfo.email_address}>{contactInfo.email_address}</a>
+                        <br/><br/>
+                        <SocialMedia />
+                    </div>
+                </div>
+                <div className="contact-image-div">
+                    <img alt="Saad Working" src={require(`../../assets/images/${isDark ? 'contactMailDark.png' : 'contactMail.png'}`)}></img>
+                </div>
             </div>
           </div>
           <div className="contact-image-div">
