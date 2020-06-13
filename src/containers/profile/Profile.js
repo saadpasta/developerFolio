d9c9941948c6e847df5fbb7080ab24a52d523a40
Saad Pasta <saadpasta70@gmail.com>
2020-06-13 16:09:27 +0500

    Remove : Axios and Replace API call with GraphQl (#184)
    * Remove : Axios and Replace API call with GraphQl

--- src/containers/profile/Profile.js
+++ src/containers/profile/Profile.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
 import GithubProfileCard from "../../components/githubProfileCard/GithubProfileCard";
-import axios from "axios";
+import ApolloClient, { gql } from "apollo-boost";
 import { openSource } from "../../portfolio";
 import Contact from "../contact/Contact";
-
 
 export default function Profile() {
   const [prof, setrepo] = useState([]);
@@ -11,17 +10,46 @@ export default function Profile() {
     setrepo(array);
   }
   function getProfileData() {
-    const client=axios.get("https://api.github.com/users/"+openSource.githubUserName)
-      .then(function(response){
-            setProfileFunction(response.data);
+    const client = new ApolloClient({
+      uri: "https://api.github.com/graphql",
+      request: (operation) => {
+        operation.setContext({
+          headers: {
+            authorization: `Bearer ${openSource.githubConvertedToken}`,
+          },
+        });
+      },
     });
+
+    client
+      .query({
+        query: gql`
+      {
+        user(login:"${openSource.githubUserName}") { 
+          name
+          bio
+          isHireable
+          avatarUrl
+          location
+        }
+    }
+      `,
+      })
+      .then((result) => {
+        setProfileFunction(result.data.user);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
   useEffect(() => {
-    getProfileData();
+    if (openSource.showGithubProfile === "true") {
+      getProfileData();
+    }
   }, []);
-  if (openSource.showGithubProfile === "true"){
-    return ( <GithubProfileCard prof={prof} key={prof.id} /> );
+  if (openSource.showGithubProfile === "true") {
+    return <GithubProfileCard prof={prof} key={prof.id} />;
   } else {
-    return(<Contact />);
+    return <Contact />;
   }
 }
