2f750d1ba447d4d5b195e0d98c8da169dbbf4206
MuhammadHasham23 <muhammad.hasham.2311@gmail.com>
2020-07-14 17:45:30 +0500

    minor fixes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -25,6 +25,14 @@ function Header() {
           <li>
             <a href="#skills">Skills</a>
           </li>
+          {exp === true && (
+            <li>
+              <a href="#experience">Work Experiences</a>
+            </li>
+          )}
+          <li>
+            <a href="#opensource">Open Source</a>
+          </li>
           <li>
             <a href="#achievements">Achievements</a>
           </li>
