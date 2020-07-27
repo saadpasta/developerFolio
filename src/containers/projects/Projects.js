a3ab861739bc0ba17d138cc8e78c6f4588ad5f7d
Saad Pasta <saadpasta70@gmail.com>
2020-07-27 08:16:56 -0700

    Update Projects.js

--- src/containers/projects/Projects.js
+++ src/containers/projects/Projects.js
@@ -1,14 +1,12 @@
-import React, { useState, useEffect, lazy, Suspense,useContext } from "react";
+import React, { useState, useEffect, useContext, Suspense, lazy } from "react";
 import ApolloClient from "apollo-boost";
 import { gql } from "apollo-boost";
 import "./Project.css";
 import Button from "../../components/button/Button";
-import Loading from "../loading/Loading";
-import { openSource, socialMediaLinks } from "../../portfolio";
-
+import { openSource,socialMediaLinks  } from "../../portfolio";
 import { Fade } from "react-reveal";
-import StyleConsumer from "../../contexts/StyleContext";
-
+import { StyleConsumer } from "../../contexts/StyleContext"
+import Loading from '../../containers/loading/Loading'
 export default function Projects() {
   const GithubRepoCard = lazy(() => import('../../components/githubRepoCard/GithubRepoCard'));
   const FailedLoading = () => null;
@@ -19,6 +17,7 @@ export default function Projects() {
     getRepoData();
   }, []);
 
+  
   function getRepoData() {
     const client = new ApolloClient({
       uri: "https://api.github.com/graphql",
@@ -76,22 +75,21 @@ export default function Projects() {
   function setrepoFunction(array) {
     setrepo(array);
   }
-  if (!(typeof repo === 'string' || repo instanceof String)) {
+  if (!(typeof repo === 'string' || repo instanceof String)){
     return (
-      <Suspense>
-        <Fade bottom duration={1000} distance="20px">
-          <div className="main" id="opensource">
-            <h1 className="project-title">Open Source Projects</h1>
-            <div className="repo-cards-div-main">
-              {repo.map((v, i) => {
-                return <GithubRepoCard isDark={isDark} repo={v} key={v.node.id} />;
-              })}
-            </div>
+      <Suspense fallback={renderLoader()}>
+        <div className="main" id="opensource">
+          <h1 className="project-title">Open Source Projects</h1>
+          <div className="repo-cards-div-main">
+            {repo.map((v, i) => {
+              return <GithubRepoCard repo={v} key={v.node.id} isDark={isDark}/>;
+            })}
           </div>
-        </Fade>
+          <Button text={"More Projects"} className="project-button" href={socialMediaLinks.github} newTab={true} />
+        </div>
       </Suspense>
     );
-  } else {
-    return (<FailedLoading />);
+  } else{
+      return(<FailedLoading />);
+    }
   }
-}
