0d0d9fa21ae05f45d98206e9b069c262471677d5
Saad Pasta <saadpasta70@gmail.com>
2020-07-29 05:33:37 -0700

    Update src/containers/skills/Skills.js
    Co-authored-by: Naveen M K <naveen@syrusdark.website>

--- src/containers/skills/Skills.js
+++ src/containers/skills/Skills.js
@@ -19,37 +19,14 @@ export default function Skills() {
           </div>
         </Fade>
         <Fade right duration={1000}>
-          <div className={"skills-text-div"}>
-            <h1
-              className={isDark ? "dark-mode skills-heading" : "skills-heading"}
-            >
-              {skillsSection.title}{" "}
-            </h1>
-            <p
-              className={
-                isDark
-                  ? "dark-mode subTitle skills-text-subtitle"
-                  : "subTitle skills-text-subtitle"
-              }
-            >
-              {skillsSection.subTitle}
-            </p>
-            <SoftwareSkill />
-            <div>
-              {skillsSection.skills.map((skills) => {
-                return (
-                  <p
-                    className={
-                      isDark
-                        ? "dark-mode subTitle skills-text"
-                        : "subTitle skills-text"
-                    }
-                  >
-                    {skills}
-                  </p>
-                );
-              })}
-            </div>
+        <div className="skills-text-div">
+          <h1 className={isDark ? "dark-mode skills-heading":"skills-heading"}>{skillsSection.title} </h1>
+          <p className={isDark ? "dark-mode subTitle skills-text-subtitle":"subTitle skills-text-subtitle"}>{skillsSection.subTitle}</p>
+          <SoftwareSkill />
+          <div>
+            {skillsSection.skills.map(skills => {
+              return <p className={isDark ? "dark-mode subTitle skills-text":"subTitle skills-text"}>{skills}</p>;
+            })}
           </div>
         </Fade>
       </div>
