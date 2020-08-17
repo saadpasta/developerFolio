e77f51aa462bcea3fecc75f5dd1ee410947e0d16
saadpasta <saadpasta70@gmail.com>
2020-08-17 04:56:19 +0000

    Deploying to gh-pages from  @ d2c7b0eaacb675b601d02e83f79f429329b63f1f 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.926e427c937bab5828d19f5b01b9d52e.js"
+  "/precache-manifest.06ba0bd486d21d4b987116def00b8339.js"
 );
 
 self.addEventListener('message', (event) => {
