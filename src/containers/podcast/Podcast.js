ca115763eb55180707fd35ed2b5b86b0183ace00
Ahmed Waqas <Ahmedwaqasnasir818@gmail.com>
2020-08-30 14:45:56 +0500

    add warning when podcast does not load

--- src/containers/podcast/Podcast.js
+++ src/containers/podcast/Podcast.js
@@ -3,10 +3,21 @@ import "./Podcast.css";
 import { podcastSection } from "../../portfolio";
 import { Fade } from "react-reveal";
 
+const cantDisplayError = "<div class='centerContent'><h2>Can't load? Check privacy protection settings</h2></div>";
+
+function timeOut(){
+  setTimeout(function() {
+    if (!(document.getElementById('podcasts').innerHTML.includes('iframe'))){
+      document.getElementById(`podcasts`).innerHTML = cantDisplayError ;
+    }
+  },
+  10000);
+}
+
 export default function Podcast() {
   return (
     <Fade bottom duration={1000} distance="20px">
-    <div className="main">
+    <div className="main" id="podcasts">
       <div className="podcast-header">
         <h1 className="podcast-header-title">{podcastSection.title}</h1>
         <p className="subTitle podcast-header-subtitle">{podcastSection.subtitle}</p>
@@ -15,7 +26,7 @@ export default function Podcast() {
         {podcastSection.podcast.map(podcastLink => {
           return (
             <div>
-              <iframe className= "podcast" src={podcastLink} frameborder="0" scrolling="no"></iframe>
+              <iframe className= "podcast" src={podcastLink} frameborder="0" scrolling="no" onLoad={timeOut}></iframe>
             </div>
           );
         })}
