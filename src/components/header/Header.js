453390c772e4665cd2968279361348fa3cd7e46d
MuhammadHasham23 <muhammad.hasham.2311@gmail.com>
2020-09-01 08:22:29 +0500

    added prescribed changes for dark mode

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,31 +5,34 @@ import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 import StyleContext from "../../contexts/StyleContext";
 import { greeting, workExperiences } from "../../portfolio";
 
-
 function Header() {
   const { isDark } = useContext(StyleContext);
   const exp = workExperiences.viewExperiences;
   return (
     <Headroom>
-      <header className={isDark ? "dark-menu header": "header"}>
+      <header className={isDark ? "dark-menu header" : "header"}>
         <a href="" className="logo">
           <span className="grey-color"> &lt;</span>
           <span className="logo-name">{greeting.username}</span>
           <span className="grey-color">/&gt;</span>
         </a>
         <input className="menu-btn" type="checkbox" id="menu-btn" />
-        <label className="menu-icon" htmlFor="menu-btn">
-          <span className="navicon"></span>
+        <label
+          className="menu-icon"
+          htmlFor="menu-btn"
+          style={{ color: "white" }}
+        >
+          <span className={isDark ? "navicon navicon-dark" : "navicon"}></span>
         </label>
         <ul className={isDark ? "dark-menu menu" : "menu"}>
           <li>
             <a href="#skills">Skills</a>
           </li>
-          {exp === true &&
+          {exp === true && (
             <li>
               <a href="#experience">Work Experiences</a>
             </li>
-          }
+          )}
           <li>
             <a href="#opensource">Open Source</a>
           </li>
@@ -46,10 +49,11 @@ function Header() {
             <a href="#contact">Contact Me</a>
           </li>
           <li>
-            <a><ToggleSwitch /></a>
+            <a>
+              <ToggleSwitch />
+            </a>
           </li>
         </ul>
-
       </header>
     </Headroom>
   );
