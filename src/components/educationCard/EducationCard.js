063e2bc5d86278329aaa6f2a3849371b68f00901
Jay <jayliu925@gmail.com>
2020-09-19 21:39:21 -0600

    Re-design style of education section

--- src/components/educationCard/EducationCard.js
+++ src/components/educationCard/EducationCard.js
@@ -1,41 +1,39 @@
-import React, { useState, createRef } from "react";
-import "./EducationCard.css";
-import ColorThief from "colorthief";
+import React, { createRef } from 'react';
+import './EducationCard.css';
 
 export default function EducationCard({ school }) {
-  const [colorArrays, setColorArrays] = useState([]);
   const imgRef = createRef();
 
-  function getColorArrays() {
-    const colorThief = new ColorThief();
-    setColorArrays(colorThief.getColor(imgRef.current));
-  }
-
-  function rgb(values) {
-    return typeof values === "undefined" ? null : "rgb(" + values.join(', ') + ")";
-  }
-
   const GetDescBullets = ({ descBullets }) => {
-    return descBullets ? descBullets.map((item) => <li className="subTitle">{item}</li>) : null
+    return descBullets
+      ? descBullets.map((item) => <li className="subTitle">{item}</li>)
+      : null;
   };
 
   return (
     <div className="education-card">
-      <div style={{background: rgb(colorArrays) }} className="education-banner">
-        <div className="education-blurred_div"></div>
-        <div className="education-div-school">
-          <h5 className="education-text-school">{school.schoolName}</h5>
-        </div>
-        <img crossOrigin={"anonymous"} ref={imgRef} className="education-roundedimg" src={school.logo} alt={school.schoolName} onLoad={() => getColorArrays()}/>
+      <div className="eduecation-card-left">
+        <img
+          crossOrigin={'anonymous'}
+          ref={imgRef}
+          className="education-roundedimg"
+          src={school.logo}
+          alt={school.schoolName}
+        />
       </div>
+      <div className="eduecation-card-right">
+        <h5 className="education-text-school">{school.schoolName}</h5>
 
-      <div className="education-text-details">
-        <h5 className="education-text-degree">{school.degree}</h5>
-        <h5 className="education-text-duration">{school.duration}</h5>
-        <p className="subTitle education-text-desc">{school.desc}</p>
-        <ul>
-          <GetDescBullets descBullets={school.descBullets} />
-        </ul>
+        <div className="education-text-details">
+          <h5 className="education-text-degree">{school.degree}</h5>
+          <h5 className="education-text-duration">{school.duration}</h5>
+          <p className="education-text-desc">{school.desc}</p>
+          <div className="education-text-bullets">
+            <ul>
+              <GetDescBullets descBullets={school.descBullets} />
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/containers/education/Education.js
+++ src/containers/education/Education.js
@@ -8,7 +8,7 @@ export default function Education() {
   return (
     <div className="education-main-div" id="education">
       <Fade left duration={1000}>
-        <div className="education-text-div">
+        <div>
           <h1 className="education-heading">{educationInfo.title}</h1>
           <div className="education-card-div">
             {educationInfo.schools.map((school) => (
