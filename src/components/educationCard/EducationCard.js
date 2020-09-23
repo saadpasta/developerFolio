2b7b2b91ea64a32785cee9c76807d3a303a7f74f
Jay <jayliu925@gmail.com>
2020-09-23 00:30:02 -0600

    Change education border color and add transitions

--- src/components/educationCard/EducationCard.js
+++ src/components/educationCard/EducationCard.js
@@ -1,4 +1,5 @@
 import React, { createRef } from 'react';
+import { Fade, Slide, Zoom } from 'react-reveal';
 import './EducationCard.css';
 
 export default function EducationCard({ school }) {
@@ -11,30 +12,37 @@ export default function EducationCard({ school }) {
   };
 
   return (
-    <div className="education-card">
-      <div className="education-card-left">
-        <img
-          crossOrigin={'anonymous'}
-          ref={imgRef}
-          className="education-roundedimg"
-          src={school.logo}
-          alt={school.schoolName}
-        />
-      </div>
-      <div className="education-card-right">
-        <h5 className="education-text-school">{school.schoolName}</h5>
+    <div>
+      <Fade left duration={1000}>
+        <div className="education-card">
+          <div className="education-card-left">
+            <img
+              crossOrigin={'anonymous'}
+              ref={imgRef}
+              className="education-roundedimg"
+              src={school.logo}
+              alt={school.schoolName}
+            />
+          </div>
+          <div className="education-card-right">
+            <h5 className="education-text-school">{school.schoolName}</h5>
 
-        <div className="education-text-details">
-          <h5 className="education-text-degree">{school.degree}</h5>
-          <p className="education-text-duration">{school.duration}</p>
-          <p className="education-text-desc">{school.desc}</p>
-          <div className="education-text-bullets">
-            <ul>
-              <GetDescBullets descBullets={school.descBullets} />
-            </ul>
+            <div className="education-text-details">
+              <h5 className="education-text-degree">{school.degree}</h5>
+              <p className="education-text-duration">{school.duration}</p>
+              <p className="education-text-desc">{school.desc}</p>
+              <div className="education-text-bullets">
+                <ul>
+                  <GetDescBullets descBullets={school.descBullets} />
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </Fade>
+      <Slide left duration={2000}>
+        <div className="education-card-border"></div>
+      </Slide>
     </div>
   );
 }
--- src/containers/education/Education.js
+++ src/containers/education/Education.js
@@ -2,21 +2,18 @@ import React from 'react';
 import './Education.css';
 import EducationCard from '../../components/educationCard/EducationCard';
 import { educationInfo } from '../../portfolio';
-import { Fade } from 'react-reveal';
 
 export default function Education() {
   return (
     <div className="education-main-div" id="education">
-      <Fade left duration={1000}>
-        <div>
-          <h1 className="education-heading">{educationInfo.title}</h1>
-          <div className="education-card-div">
-            {educationInfo.schools.map((school) => (
-              <EducationCard school={school} />
-            ))}
-          </div>
+      <div>
+        <h1 className="education-heading">{educationInfo.title}</h1>
+        <div className="education-card-div">
+          {educationInfo.schools.map((school) => (
+            <EducationCard school={school} />
+          ))}
         </div>
-      </Fade>
+      </div>
     </div>
   );
 }
