d048058c63b550a02ec2671e84356524de1d87fe
MuhammadHasham23 <muhammad.hasham.2311@gmail.com>
2020-10-02 19:21:43 +0500

    fixed multiple github cards

--- src/components/githubRepoCard/GithubRepoCard.js
+++ src/components/githubRepoCard/GithubRepoCard.js
@@ -16,7 +16,7 @@ export default function GithubRepoCard({ repo, isDark }) {
           key={repo.node.id}
           onClick={() => openRepoinNewTab(repo.node.url)}
         >
-          <div style={{ display: "flex", alignItems: "baseline" }}>
+          <div className="repo-name-div">
             <svg
               aria-hidden="true"
               className="octicon"
@@ -36,13 +36,15 @@ export default function GithubRepoCard({ repo, isDark }) {
           <p className="repo-description">{repo.node.description}</p>
           <div className="repo-stats">
             <div className="repo-left-stat">
-              <span>
-                <div
-                  className="language-color"
-                  style={{ backgroundColor: repo.node.primaryLanguage.color }}
-                ></div>
-                <p>{repo.node.primaryLanguage.name}</p>
-              </span>
+              {repo.node.primaryLanguage !== null && (
+                <span>
+                  <div
+                    className="language-color"
+                    style={{ backgroundColor: repo.node.primaryLanguage.color }}
+                  ></div>
+                  <p>{repo.node.primaryLanguage.name}</p>
+                </span>
+              )}
               <span>
                 <svg
                   aria-hidden="true"
@@ -80,60 +82,8 @@ export default function GithubRepoCard({ repo, isDark }) {
                 <p>{repo.node.stargazers.totalCount}</p>
               </span>
             </div>
-            <p className="repo-description">{repo.node.description}</p>
-            <div className="repo-stats">
-              <div className="repo-left-stat">
-                {repo.node.primaryLanguage !== null && (
-                  <span>
-                    <div
-                      className="language-color"
-                      style={{
-                        backgroundColor: repo.node.primaryLanguage.color,
-                      }}
-                    ></div>
-                    <p>{repo.node.primaryLanguage.name}</p>
-                  </span>
-                )}
-                <span>
-                  <svg
-                    aria-hidden="true"
-                    className="octicon"
-                    height="20"
-                    role="img"
-                    viewBox="0 0 10 16"
-                    width="12"
-                    fill="rgb(106, 115, 125)"
-                    className="repo-star-svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M8 1a1.993 1.993 0 0 0-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 0 0 2 1a1.993 1.993 0 0 0-1 3.72V6.5l3 3v1.78A1.993 1.993 0 0 0 5 15a1.993 1.993 0 0 0 1-3.72V9.5l3-3V4.72A1.993 1.993 0 0 0 8 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"
-                    ></path>
-                  </svg>
-                  <p>{repo.node.forkCount}</p>
-                </span>
-                <span>
-                  <svg
-                    aria-hidden="true"
-                    className="octicon"
-                    height="20"
-                    role="img"
-                    viewBox="0 0 14 16"
-                    width="14"
-                    fill="rgb(106, 115, 125)"
-                    className="repo-star-svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"
-                    ></path>
-                  </svg>
-                  <p>{repo.node.stargazers.totalCount}</p>
-                </span>
-              </div>
-              <div className="repo-right-stat">
-                <p>{repo.node.diskUsage} KB</p>
-              </div>
+            <div className="repo-right-stat">
+              <p>{repo.node.diskUsage} KB</p>
             </div>
           </div>
         </div>
