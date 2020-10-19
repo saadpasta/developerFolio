08640e2c58af2779a269972b077abac99e6dbf42
Samrat De <desamrat31@gmail.com>
2020-10-19 16:36:38 +0530

    removed prettier config from the files

--- src/containers/projects/Projects.js
+++ src/containers/projects/Projects.js
@@ -7,97 +7,93 @@ import Loading from '../loading/Loading'
 import { openSource, socialMediaLinks } from '../../portfolio'
 
 export default function Projects() {
-	const GithubRepoCard = lazy(() =>
-		import('../../components/githubRepoCard/GithubRepoCard')
-	)
-	const FailedLoading = () => null
-	const renderLoader = () => <Loading />
-	const [repo, setrepo] = useState([])
+  const GithubRepoCard = lazy(() =>
+    import('../../components/githubRepoCard/GithubRepoCard')
+  )
+  const FailedLoading = () => null
+  const renderLoader = () => <Loading />
+  const [repo, setrepo] = useState([])
 
-	useEffect(() => {
-		getRepoData()
-	}, [])
+  useEffect(() => {
+    getRepoData()
+  }, [])
 
-	function getRepoData() {
-		const client = new ApolloClient({
-			uri: 'https://api.github.com/graphql',
-			request: (operation) => {
-				operation.setContext({
-					headers: {
-						authorization: `Bearer ${openSource.githubConvertedToken}`,
-					},
-				})
-			},
-		})
+  function getRepoData() {
+    const client = new ApolloClient({
+      uri: 'https://api.github.com/graphql',
+      request: (operation) => {
+        operation.setContext({
+          headers: {
+            authorization: `Bearer ${openSource.githubConvertedToken}`,
+          },
+        })
+      },
+    })
 
-		client
-			.query({
-				query: gql`
-		{
-		user(login: "${openSource.githubUserName}") {
-		  pinnedItems(first: 6, types: [REPOSITORY]) {
-			totalCount
-			edges {
-			  node {
-				... on Repository {
-				  name
-				  description
-				  forkCount
-				  stargazers {
-					totalCount
-				  }
-				  url
-				  id
-				  diskUsage
-				  primaryLanguage {
-					name
-					color
-				  }
-				}
-			  }
-			}
-		  }
-		}
-	  }
-		`,
-			})
-			.then((result) => {
-				setrepoFunction(result.data.user.pinnedItems.edges)
-				// console.log(result)
-			})
-			.catch(function (error) {
-				console.log(error)
-				setrepoFunction('Error')
-				console.log(
-					'Error in calling the github api using username. Set your github token and username properly.'
-				)
-			})
-	}
+    client.query({
+      query: gql`
+      {
+        user(login: "${openSource.githubUserName}") {
+          pinnedItems(first: 6, types: [REPOSITORY]) {
+            totalCount
+            edges {
+              node {
+                ... on Repository {
+                  name
+                  description
+                  forkCount
+                  stargazers {
+                    totalCount
+                  }
+                  url
+                  id
+                  diskUsage
+                  primaryLanguage {
+                    name
+                    color
+                  }
+                }
+              }
+            }
+          }
+        }
+      }`,
+    }).then((result) => {
+        setrepoFunction(result.data.user.pinnedItems.edges)
+        // console.log(result)
+    })
+    .catch(function (error) {
+      console.log(error)
+      setrepoFunction('Error')
+      console.log('Error in calling the github api using username. Set your github token and username properly.')
+    })
+  }
 
-	function setrepoFunction(array) {
-		setrepo(array)
-	}
-	if (!(typeof repo === 'string' || repo instanceof String)) {
-		return (
-			<Suspense fallback={renderLoader()}>
-				<div className='main' id='opensource'>
-					<h1 className='project-title'>Open Source Projects</h1>
-					<div className='repo-cards-div-main'>
-						{repo.map((v, i) => {
-							return <GithubRepoCard repo={v} key={v.node.id} />
-						})}
-					</div>
-					<Button
-						text={'More Projects'}
-						className='project-button'
-						href={socialMediaLinks.github}
-						newTab={true}
-					/>
-				</div>
-			</Suspense>
-		)
-	} else {
-		console.log('Nothing is shown in place of Projects section.')
-		return <FailedLoading />
-	}
+  function setrepoFunction(array) {
+    setrepo(array)
+  }
+  
+  if (!(typeof repo === 'string' || repo instanceof String)) {
+    return (
+      <Suspense fallback={renderLoader()}>
+        <div className='main' id='opensource'>
+          <h1 className='project-title'>Open Source Projects</h1>
+          <div className='repo-cards-div-main'>
+            {repo.map((v, i) => {
+              return <GithubRepoCard repo={v} key={v.node.id} />
+            })}
+          </div>
+          <Button
+            text={'More Projects'}
+            className='project-button'
+            href={socialMediaLinks.github}
+            newTab={true}
+          />
+        </div>
+      </Suspense>
+    )
+  } else {
+    console.log('Nothing is shown in place of Projects section.')
+    return <FailedLoading />
+  }
 }
--- src/containers/twitter-embed/twitter.js
+++ src/containers/twitter-embed/twitter.js
@@ -5,40 +5,39 @@ import { TwitterTimelineEmbed } from 'react-twitter-embed'
 import { twitterDetails } from '../../portfolio'
 
 const renderLoader = () => <Loading />
-const cantDisplayError =
-	"<div class='centerContent'><h2>Can't load? Check privacy protection settings</h2></div>"
+const cantDisplayError = "<div class='centerContent'><h2>Can't load? Check privacy protection settings</h2></div>"
 
 function timeOut() {
-	setTimeout(function () {
-		if (!document.getElementById('twitter').innerHTML.includes('iframe')) {
-			console.log("Can't load? Check privacy protection settings")
-			document.getElementById('twitter').innerHTML = cantDisplayError
-		}
-	}, 10000)
+  setTimeout(function () {
+    if (!document.getElementById('twitter').innerHTML.includes('iframe')) {
+      console.log("Can't load? Check privacy protection settings")
+      document.getElementById('twitter').innerHTML = cantDisplayError
+    }
+  }, 10000)
 }
 var widthScreen = window.screen.width
 export default function Twitter() {
-	if (twitterDetails.userName) {
-		return (
-			<Suspense fallback={renderLoader()}>
-				<div class='tw-main-div' id='twitter'>
-					<div className='centerContent'>
-						<TwitterTimelineEmbed
-							sourceType='profile'
-							screenName={twitterDetails.userName}
-							options={{ height: 400, width: { widthScreen } }}
-							placeholder={renderLoader()}
-							autoHeight={false}
-							borderColor='#fff'
-							noFooter={true}
-							onload={timeOut()}
-						/>
-					</div>
-				</div>
-			</Suspense>
-		)
-	} else {
-	  console.log('Twitter Username is empty !')
-		return null
-	}
+  if (twitterDetails.userName) {
+    return (
+      <Suspense fallback={renderLoader()}>
+        <div class='tw-main-div' id='twitter'>
+          <div className='centerContent'>
+            <TwitterTimelineEmbed
+              sourceType='profile'
+              screenName={twitterDetails.userName}
+              options={{ height: 400, width: { widthScreen } }}
+              placeholder={renderLoader()}
+              autoHeight={false}
+              borderColor='#fff'
+              noFooter={true}
+              onload={timeOut()}
+            />
+          </div>
+        </div>
+      </Suspense>
+    )
+  } else {
+    console.log('Twitter Username is empty !')
+    return null
+  }
 }
