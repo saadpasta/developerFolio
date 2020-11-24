60217695c59a3639f58618fdb071448aff1493f5
james-muriithi <muriithijames556@gmail.com>
2020-11-24 11:03:57 +0300

    dark mode toggle button

--- src/components/ToggleSwitch/ToggleSwitch.js
+++ src/components/ToggleSwitch/ToggleSwitch.js
@@ -1,22 +1,56 @@
 import React, { useState, useContext } from "react";
+import Toggle from "react-toggle"
+
 import StyleContext from "../../contexts/StyleContext";
 import "./ToggleSwitch.css";
+// icons
+import sun from "../../assets/images/sun.png"
+import moon from "../../assets/images/moon.png"
+
 const ToggleSwitch = () => {
   const [isChecked, setChecked] = useState(false);
   const styleContext = useContext(StyleContext);
 
+
   return (
-    <label className="switch">
-      <input
-        type="checkbox"
+    <Toggle
+        // defaultChecked={state.isDark}
+        icons={{
+          checked: (
+            <img
+              src={moon}
+              width="16"
+              height="16"
+              alt="moon"
+              style={{
+                pointerEvents: "none",
+                width: "16px",
+                heigth: "16px",
+              }}
+            />
+          ),
+          unchecked: (
+            <img
+              src={sun}
+              width="16"
+              height="16"
+              alt="sun"
+              style={{
+                pointerEvents: "none",
+                width: "16px",
+                heigth: "16px",
+              }}
+            />
+          ),
+        }}
+        aria-label="toggle dark mode"
         checked={isChecked}
         onChange={() => {
           styleContext.changeTheme();
           setChecked(!isChecked);
         }}
       />
-      <span className="slider round"></span>
-    </label>
   );
+
 };
 export default ToggleSwitch;
