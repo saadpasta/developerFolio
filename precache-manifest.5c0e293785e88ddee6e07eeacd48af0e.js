a4d5e4f6c5b1fb5f178dc220375b00cee9777c28
righved.k <righved.k@media.net>
2020-11-27 05:51:14 +0530

    Updates

---
+++ precache-manifest.5c0e293785e88ddee6e07eeacd48af0e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab30e28698387ba77a8f0e1cca998eee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4abf78bc25d122a3d40b",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "ca66c4d5cd9bbac943be",
+    "url": "/static/css/4.a922a401.chunk.css"
+  },
+  {
+    "revision": "cefa8e935ca54d4c25ab",
+    "url": "/static/css/main.bd4e58e4.chunk.css"
+  },
+  {
+    "revision": "48cb989e553a2553c44d",
+    "url": "/static/js/2.48c636bc.chunk.js"
+  },
+  {
+    "revision": "4abf78bc25d122a3d40b",
+    "url": "/static/js/3.60be7405.chunk.js"
+  },
+  {
+    "revision": "ca66c4d5cd9bbac943be",
+    "url": "/static/js/4.d85a818a.chunk.js"
+  },
+  {
+    "revision": "cefa8e935ca54d4c25ab",
+    "url": "/static/js/main.33c53342.chunk.js"
+  },
+  {
+    "revision": "9235b36240575a8bfde6",
+    "url": "/static/js/runtime-main.e1a88013.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "be9f6f01979f58d1f57479d96d709a95",
+    "url": "/static/media/book_reco.be9f6f01.jpeg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "f342014976e486a859cc41a8561fade2",
+    "url": "/static/media/me_img.f3420149.jpeg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "5d7dfab963e9ac8d652faf66e4fdc41d",
+    "url": "/static/media/uptu_logo.5d7dfab9.png"
+  }
+]);
