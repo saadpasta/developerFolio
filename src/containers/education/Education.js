db15f3ec18c39462ceff95c7dd835e6f2e399c37
Najam Shehzad <najambutt195@gmail.com>
2020-12-05 20:14:14 +0500

    Bug Fix in Education Container

--- src/containers/education/Education.js
+++ src/containers/education/Education.js
@@ -4,14 +4,17 @@ import EducationCard from '../../components/educationCard/EducationCard';
 import { educationInfo } from '../../portfolio';
 
 export default function Education() {
-  return (
-    <div className="education-section" id="education">
-        <h1 className="education-heading">Education</h1>
-        <div className="education-card-container">
-          {educationInfo.schools.map((school) => (
-            <EducationCard school={school} />
-          ))}
-        </div>
-    </div>
-  );
+   if(educationInfo.viewEducation){
+    return (
+      <div className="education-section" id="education">
+          <h1 className="education-heading">Education</h1>
+          <div className="education-card-container">
+            {educationInfo.schools.map((school) => (
+              <EducationCard school={school} />
+            ))}
+          </div>
+      </div>
+    );
+  }
+  return null;
 }
