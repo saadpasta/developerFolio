18a7786ab5c0352997ac2abf39b45db7844d5d04
Saad Pasta <saadpasta70@gmail.com>
2020-12-20 14:14:07 +0500

    put in true in string

--- src/portfolio.js
+++ src/portfolio.js
@@ -190,7 +190,7 @@ To know how to get github key look at readme.md */
 const openSource = {
   githubConvertedToken: process.env.REACT_APP_GITHUB_TOKEN,
   githubUserName: "saadpasta", // Change to your github username to view your profile in Contact Section.
-  showGithubProfile : true,  // Set true or false to show Contact profile using Github, defaults to false
+  showGithubProfile : "true",  // Set true or false to show Contact profile using Github, defaults to false
   display: true // Set true to display this section, defaults to false
 };
 
