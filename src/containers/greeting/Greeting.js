53195b21ae6b9b1620837e1868e7d08ed5e41c98
Sebastian Auner <github@mailwarden.de>
2021-03-25 09:28:35 +0100

    remove wave emoji

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -27,7 +27,7 @@ export default function Greeting() {
               >
                 {" "}
                 {greeting.title}{" "}
-                <span className="wave-emoji">{emoji("👋")}</span>
+                {/* <span className="wave-emoji">{emoji("👋")}</span> */}
               </h1>
               <p
                 className={
