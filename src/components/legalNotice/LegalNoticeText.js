7741abb5afb3dd058a6869e28a910cef1c5066f4
Ctrl-A-Del <github@mailwarden.de>
2021-04-10 22:09:49 +0200

    tiny style adjustments

--- src/components/legalNotice/LegalNoticeText.js
+++ src/components/legalNotice/LegalNoticeText.js
@@ -6,6 +6,7 @@ export default function LegalNoticeText() {
       <h1>Legal Disclosure</h1>
       Information in accordance with Section 5 TMG
       <br />
+      <br />
       Sebastian Auner
       <br />
       Naphausen 22
