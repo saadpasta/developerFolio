44ec316d9acbde1125f10b5834d8f9add8ed3bdf
DittoGod <DittoGod@users.noreply.github.com>
2021-05-20 20:38:44 +0200

    attemp to get opensource to show

--- src/portfolio.js
+++ src/portfolio.js
@@ -203,10 +203,10 @@ const workExperiences = {
 To know how to get github key look at readme.md */
 
 const openSource = {
-  githubConvertedToken: process.env.REACT_APP_GITHUB_TOKEN,
-  githubUserName: "DittoGod", // Change to your github username to view your profile in Contact Section.
+  // githubConvertedToken: process.env.REACT_APP_GITHUB_TOKEN,
+  // githubUserName: "DittoGod", // Change to your github username to view your profile in Contact Section.
   showGithubProfile: "false", // Set true or false to show Contact profile using Github, defaults to true
-  display: false // Set false to hide this section, defaults to true
+  display: true // Set false to hide this section, defaults to true
 };
 
 // Some big projects you have worked on
