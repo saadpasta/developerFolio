34266057fa003c8852a95fb7049972efb0000c03
DittoGod <DittoGod@users.noreply.github.com>
2021-05-20 15:43:54 +0200

    update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -205,7 +205,7 @@ To know how to get github key look at readme.md */
 const openSource = {
   githubConvertedToken: process.env.REACT_APP_GITHUB_TOKEN,
   githubUserName: "DittoGod", // Change to your github username to view your profile in Contact Section.
-  showGithubProfile: "false" // Set true or false to show Contact profile using Github, defaults to false
+  showGithubProfile: "false", // Set true or false to show Contact profile using Github, defaults to false
   display: true // Set false to hide this section, defaults to true
 };
 
