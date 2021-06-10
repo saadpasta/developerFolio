42cd3ec437c64d0077e4f65c3b88d818cf62cdb9
Vivek Kasture <vivek_kasture@techjoomla.com>
2021-06-10 18:14:58 +0530

    Updated portfolio

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -9,9 +9,29 @@ import Button from "../../components/button/Button";
 
 import {illustration, greeting} from "../../portfolio";
 import StyleContext from "../../contexts/StyleContext";
+import anime from "animejs";
 
 export default function Greeting() {
   const {isDark} = useContext(StyleContext);
+  const animationRef = React.useRef(null);
+  React.useEffect(() => {
+    animationRef.current = anime
+      .timeline({loop: true})
+      .add({
+        targets: ".ml6 .letter",
+        translateY: ["1.1em", 0],
+        translateZ: 0,
+        duration: 750,
+        delay: (el, i) => 50 * i
+      })
+      .add({
+        targets: ".ml6",
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+      });
+  }, []);
   if (!greeting.displayGreeting) {
     return null;
   }
@@ -21,22 +41,31 @@ export default function Greeting() {
         <div className="greeting-main">
           <div className="greeting-text-div">
             <div>
-              <h1
+              {/* <h1
                 className={isDark ? "dark-mode greeting-text" : "greeting-text"}
               >
                 {" "}
                 {greeting.title}{" "}
                 <span className="wave-emoji">{emoji("👋")}</span>
-              </h1>
-              <p
+              </h1> */}
+              <h1
                 className={
-                  isDark
-                    ? "dark-mode greeting-text-p"
-                    : "greeting-text-p subTitle"
+                  isDark ? "ml6 dark-mode greeting-text" : "ml6 greeting-text"
                 }
               >
-                {greeting.subTitle}
-              </p>
+                <span className="text-wrapper">
+                  <span className="letters">
+                    {[...greeting.title].map((name, index) => {
+                      return (
+                        <span className="letter" key={index}>
+                          {name}
+                        </span>
+                      );
+                    })}
+                  </span>
+                </span>
+              </h1>
+              {greeting.subTitle}
               <SocialMedia />
               <div className="button-greeting-div">
                 <Button text="Contact me" href="#contact" />
