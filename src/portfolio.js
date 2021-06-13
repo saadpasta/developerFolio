0ad7bea72f9ca1cb3863387e4f3d102004435c78
AN <jhan891012@gmail.com>
2021-06-13 11:59:48 +0900

    first commit

--- src/portfolio.js
+++ src/portfolio.js
@@ -11,10 +11,10 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "JUHYUN AN",
+  title: "Hi all, I'm JUHYUN SIMON AN",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "A passionate Software Developer 🚀 having an experience of building Web with JavaScript and some other cool libraries and frameworks."
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing",
