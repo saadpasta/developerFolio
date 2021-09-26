30533e90e8e855a1052e5930852e0b57cc69e023
Elliot Alderson <77377704+sc4rygh0st@users.noreply.github.com>
2021-09-26 17:25:13 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -8,7 +8,7 @@ import emoji from "react-easy-emoji";
 const greeting = {
   /* Your Summary And Greeting Section */
   username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  title: "Hi all, I'm Nihal",
   subTitle: emoji("A passionate Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."),
   resumeLink: "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing"
 };
