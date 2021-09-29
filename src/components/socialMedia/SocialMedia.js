afc5723d585a2de5c939c05cac91a7984b369c6b
Sunit Roy <iamsunitroy03@gmail.com>
2021-09-29 22:30:25 +0530

    Change email logo (#421)

--- src/components/socialMedia/SocialMedia.js
+++ src/components/socialMedia/SocialMedia.js
@@ -39,7 +39,7 @@ export default function socialMedia() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <i className="fab fa-google"></i>
+          <i className="fas fa-envelope"></i>
           <span></span>
         </a>
       ) : null}
