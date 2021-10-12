8756244a40d32a14eb30d1522577e86a5454a0e4
S-codes14 <smlmnguni14@gmail.com>
2021-10-12 22:33:14 +0200

    it works added solution to https://github.com/saadpasta/developerFolio/issues/415

--- src/containers/workExperience/WorkExperience.js
+++ src/containers/workExperience/WorkExperience.js
@@ -4,9 +4,26 @@ import ExperienceCard from "../../components/experienceCard/ExperienceCard";
 import {workExperiences} from "../../portfolio";
 import {Fade} from "react-reveal";
 import StyleContext from "../../contexts/StyleContext";
+import {Carousel} from '3d-react-carousal';
 
 export default function WorkExperience() {
   const {isDark} = useContext(StyleContext);
+  let slides = workExperiences.experience.map((card, i) => {
+    return (
+      <ExperienceCard
+        key={i}
+        isDark={isDark}
+        cardInfo={{
+          company: card.company,
+          desc: card.desc,
+          date: card.date,
+          companylogo: card.companylogo,
+          role: card.role,
+          descBullets: card.descBullets
+        }}
+      />
+    );
+  })
   if (workExperiences.display) {
     return (
       <div id="experience">
@@ -15,22 +32,7 @@ export default function WorkExperience() {
             <div>
               <h1 className="experience-heading">Experiences</h1>
               <div className="experience-cards-div">
-                {workExperiences.experience.map((card, i) => {
-                  return (
-                    <ExperienceCard
-                      key={i}
-                      isDark={isDark}
-                      cardInfo={{
-                        company: card.company,
-                        desc: card.desc,
-                        date: card.date,
-                        companylogo: card.companylogo,
-                        role: card.role,
-                        descBullets: card.descBullets
-                      }}
-                    />
-                  );
-                })}
+              <Carousel slides={slides} autoplay={true} interval={1000}/>
               </div>
             </div>
           </div>
