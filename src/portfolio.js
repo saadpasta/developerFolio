0f947cc8972afa22d40d594c6d4339fc914cc25a
Lucas Tan <sheng.x.tan@gmail.com>
2022-06-23 22:32:30 -0600

    change username

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,7 +20,7 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
+  username: "Lucas Tan",
   title: "Hi everyone, I'm Lucas",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native / Ruby / Rails / and some other cool libraries and frameworks."
