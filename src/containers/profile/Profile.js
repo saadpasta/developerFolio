1b7f851fdf143b6b97b56bf8eacbf3201496214d
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2022-08-24 16:56:30 -0500

    detach from fork

--- src/containers/profile/Profile.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import Contact from "../contact/Contact";
-
-export default function Profile() {
-    return <Contact />;
-}
--- src/containers/projects/Projects.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./Project.scss";
-export default function Projects() {
-	// todo: implement this
-	return (<div></div>)
-
-}
