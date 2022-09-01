4b65b67e2ca3a709f0c1c0b85df501d0e3e5d24b
camipozas <cpozasg1103@gmail.com>
2022-09-01 12:21:06 -0400

    the comment is improved indicating how and where it is seen

--- src/portfolio.js
+++ src/portfolio.js
@@ -374,7 +374,7 @@ const twitterDetails = {
   display: true // Set true to display this section, defaults to false
 };
 
-const isHireable = true; // Set false if you are not looking for a job
+const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
 
 export {
   illustration,
