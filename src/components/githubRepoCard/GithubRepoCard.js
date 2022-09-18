46e4bc789cf018640745970548934925a35d5bb2
kartikcho <kartikc.918@gmail.com>
2022-09-18 14:43:58 +0530

    Add number formatting to stargazer count

--- src/components/githubRepoCard/GithubRepoCard.js
+++ src/components/githubRepoCard/GithubRepoCard.js
@@ -80,7 +80,7 @@ export default function GithubRepoCard({repo, isDark}) {
                       d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"
                     ></path>
                   </svg>
-                  <p>{repo.node.stargazers.totalCount}</p>
+                  <p>{repo.node.stargazers.totalCount.toLocaleString("en-US")}</p>
                 </span>
               </div>
               <div className="repo-right-stat">
