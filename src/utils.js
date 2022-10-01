50033d598f5067d34073bc3f7db59c296f32fae8
Kartik Choudhary <chow.kartik@gmail.com>
2022-10-01 21:31:38 +0530

    Remove git diff lines

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:src/components/githubRepoCard/sizeConversion.js
-export const format = value => {
-=======
 export const formatFileSizeDisplay = (value) => {
->>>>>>> 6e82510 (Renamed and moved format function to utils.js):src/utils.js
   if (value < 1024) {
     return `${value} KB`;
   }
