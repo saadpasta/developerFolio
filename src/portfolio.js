80333372f0bda4ef00916f82eacc8c8165aec24d
Julius Philip Bartolome <juliusbartolome@hotmail.ph>
2022-12-20 00:30:37 +0800

    updated greeting part and social media links

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,26 +20,25 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Julius Philip Bartolome",
+  title: "Hi all, I'm Julius",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+          "A passionate Full Stack Software Developer 🚀 having an experience of building Automation workflows, Web applications, and Cloud infrastructure  with .NET / JavaScript / Reactjs / Nodejs / Python / Docker / Kubernetes and some other cool libraries and frameworks."
   ),
-  resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
+//  resumeLink:
+//    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  github: "https://github.com/juliusbartolome",
+  linkedin: "https://www.linkedin.com/in/juliusbartolome/",
+  gmail: "juliusbartolome14@gmail.com",
+  facebook: "https://www.facebook.com/juliusphilipbartolome",
+  stackoverflow: "https://stackoverflow.com/users/3140305/juliusbartolome",
+  //  medium: "https://medium.com/@saadpasta",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
