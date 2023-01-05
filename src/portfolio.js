fdb8b87d1ff26345a750247006bc6a9862ab5216
ViSiON-aF <chaitanya.mishra@outlook.in>
2023-01-05 21:34:00 +0530

    great code
    very nice

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,13 +20,8 @@ const illustration = {
 };
 
 const greeting = {
-<<<<<<< Updated upstream
-  username: "Chaitanya",
-  title: "Hi all, I'm Saad",
-=======
   username: "Chaitanya Mishra",
   title: "Hi all, I'm Chaitanya",
->>>>>>> Stashed changes
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
