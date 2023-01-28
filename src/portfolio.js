d4f2b3192a3d06eca5d80ddc6539342632b30d16
81CuongVn <89056222+81CuongVn@users.noreply.github.com>
2023-01-28 22:36:59 +0700

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -363,8 +363,8 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
-  email_address: "saadpasta70@gmail.com"
+  number: "+84-365629897",
+  email_address: "alan45walker12@gmail.com"
 };
 
 // Twitter Section
