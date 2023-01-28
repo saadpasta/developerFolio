965d1416d1cf7c5af5a8ab1144f24ea0f7809d14
81CuongVn <89056222+81CuongVn@users.noreply.github.com>
2023-01-28 22:44:59 +0700

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -206,8 +206,12 @@ To know how to get github key look at readme.md */
 
 const openSource = {
   showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
-  display: true // Set false to hide this section, defaults to true
+  display: true, // Set false to hide this section, defaults to true
+  footerLink: [
+  {
   url: "https://www.ianww.com/"
+}
+  ]
 };
 
 // Some big projects you have worked on
