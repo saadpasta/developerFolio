50914051af3ec5405787f335cc443ca0bd7d8213
KazAttarwala <52388168+KazAttarwala@users.noreply.github.com>
2023-01-31 21:35:52 -0500

    issue_574_move_skills_section_to_next

---
+++ src/pages/_app.js
@@ -0,0 +1,5 @@
+import '@/styles/globals.scss'
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import {Inter} from "@next/font/google";
+
+const inter = Inter({subsets: ["latin"]});
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>DeveloperFolio</title>
+      </Head>
+      <div>DeveloperFolio Migration to be done</div>
+    </>
+  );
+}
