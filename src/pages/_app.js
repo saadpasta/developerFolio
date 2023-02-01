e81200a6e55bea6dc212a47f5f1f2833f5db1517
KazAttarwala <52388168+KazAttarwala@users.noreply.github.com>
2023-01-31 21:35:52 -0500

    issue_574_move_skills_section_to_next

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
 import {Inter} from "@next/font/google";
-import styles from "@/styles/Home.module.css";
 
 const inter = Inter({subsets: ["latin"]});
 
