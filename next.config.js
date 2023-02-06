d5db5a6adf324c8f3e86e061036f317a6f6dc408
KazAttarwala <52388168+KazAttarwala@users.noreply.github.com>
2023-02-05 19:56:22 -0500

    added a config setting to support automatic camelCasing for scss files and updated the classNames for SoftwareSkills

--- next.config.js
+++ next.config.js
@@ -4,6 +4,31 @@ const nextConfig = {
   distDir: 'build',
   experimental: {
     appDir: true,
+  },  
+  //this is to support automatic camelCasing in module.scss files
+  webpack: (config) => {
+    const rules = config.module.rules
+      .find((rule) => typeof rule.oneOf === 'object').oneOf.filter((rule) => Array.isArray(rule.use));
+    rules.forEach((rule) => {
+      rule.use.forEach((moduleLoader) => {
+        if (
+          moduleLoader.loader !== undefined
+          && moduleLoader.loader.includes('css-loader')
+          && typeof moduleLoader.options.modules === 'object'
+        ) {
+          moduleLoader.options = {
+            ...moduleLoader.options,
+            modules: {
+              ...moduleLoader.options.modules,
+              // This is where we allow camelCase class names
+              exportLocalsConvention: 'camelCase'
+            }
+          };
+        }
+      });
+    });
+
+    return config;
   }
 }
 
