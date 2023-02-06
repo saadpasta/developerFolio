0402ae65ee870efa7027d6499bb7b26373e38da2
KazAttarwala <52388168+KazAttarwala@users.noreply.github.com>
2023-02-05 20:11:54 -0500

    updated Loading container to use Loading.module.scss styling

--- src/containers/loading/Loading.js
+++ src/containers/loading/Loading.js
@@ -1,22 +1,22 @@
 import React from "react";
-import "./Loading.module.scss";
+import styles from "./Loading.module.scss";
 
 export default function Loading() {
   return (
     <div className="centerContent">
-      <div className="sk-circle">
-        <div className="sk-circle1 sk-child"></div>
-        <div className="sk-circle2 sk-child"></div>
-        <div className="sk-circle3 sk-child"></div>
-        <div className="sk-circle4 sk-child"></div>
-        <div className="sk-circle5 sk-child"></div>
-        <div className="sk-circle6 sk-child"></div>
-        <div className="sk-circle7 sk-child"></div>
-        <div className="sk-circle8 sk-child"></div>
-        <div className="sk-circle9 sk-child"></div>
-        <div className="sk-circle10 sk-child"></div>
-        <div className="sk-circle11 sk-child"></div>
-        <div className="sk-circle12 sk-child"></div>
+      <div className={styles.skCircle}>
+        <div className={`${styles.skCircle1} ${styles.skChild}`}></div>
+        <div className={`${styles.skCircle2} ${styles.skChild}`}></div>
+        <div className={`${styles.skCircle3} ${styles.skChild}`}></div>
+        <div className={`${styles.skCircle4} ${styles.skChild}`}></div>
+        <div className={`${styles.skCircle5} ${styles.skChild}`}></div>
+        <div className={`${styles.skCircle6} ${styles.skChild}`}></div>
+        <div className={`${styles.skCircle7} ${styles.skChild}`}></div>
+        <div className={`${styles.skCircle8} ${styles.skChild}`}></div>
+        <div className={`${styles.skCircle9} ${styles.skChild}`}></div>
+        <div className={`${styles.skCircle10} ${styles.skChild}`}></div>
+        <div className={`${styles.skCircle11} ${styles.skChild}`}></div>
+        <div className={`${styles.skCircle12} ${styles.skChild}`}></div>
       </div>
     </div>
   );
