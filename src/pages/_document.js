d8bccc4efb728b78f952fda1410480e55ee408fc
KazAttarwala <52388168+KazAttarwala@users.noreply.github.com>
2023-02-05 22:06:12 -0500

    added fontawesome cdn link to _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          type="text/css" href="https://cdn.jsdelivr.net/gh/FortAwesome/Font-Awesome@5.15.4/css/all.min.css"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
