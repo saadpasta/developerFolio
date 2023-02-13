4c849dbf7f41874ccac51e58e709cac6cc3cc48b
KazAttarwala <52388168+KazAttarwala@users.noreply.github.com>
2023-02-13 15:11:46 -0500

    converted DisplayLottie to functional component so that it becomes a Server component instead of a Client component

--- src/components/displayLottie/DisplayLottie.js
+++ src/components/displayLottie/DisplayLottie.js
@@ -1,20 +1,18 @@
-import React, {Component, Suspense} from "react";
+import React, {Suspense} from "react";
 import Lottie from "react-lottie";
 import Loading from "../../containers/loading/Loading";
 
-export default class DisplayLottie extends Component {
-  render() {
-    const animationData = this.props.animationData;
-    const defaultOptions = {
-      loop: true,
-      autoplay: true,
-      animationData: animationData
-    };
+export default function DisplayLottie() {
+  const animationData = this.props.animationData;
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData
+  };
 
-    return (
-      <Suspense fallback={<Loading />}>
-        <Lottie options={defaultOptions} isClickToPauseDisabled={true} />
-      </Suspense>
-    );
-  }
+  return (
+    <Suspense fallback={<Loading />}>
+      <Lottie options={defaultOptions} isClickToPauseDisabled={true} />
+    </Suspense>
+  );
 }
