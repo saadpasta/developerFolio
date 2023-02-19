257ab6c7a9cb43113bdc1a192a8aa41538d7c503
rmeyer1 <robmeyer03@gmail.com>
2023-02-18 22:21:56 -0500

    App Maintenance
    Update meta tags.  Update Footer component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,15 +9,6 @@ export default function Footer() {
   return (
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by DeveloperFolio Team")}
-        </p>
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          Theme by{" "}
-          <a href="https://github.com/saadpasta/developerFolio">
-            developerFolio
-          </a>
-        </p>
       </div>
     </Fade>
   );
