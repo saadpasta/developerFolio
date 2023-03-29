ff33a3f1f4d166d1c5c7f448c4d739007b9d61b1
pthuymenhieuthu <129261145+pthuymenhieuthu@users.noreply.github.com>
2023-03-29 19:51:50 +0700

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,10 +20,10 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Thuy Nguyen",
+  title: "Hi all, I'm Thuy",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "A Social Media enthuhsiast 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
