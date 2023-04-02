d44d4e37ed110b1cc9ebec2f0f22b47d37c1d3e6
Ali <105427281+Alilock@users.noreply.github.com>
2023-04-02 16:34:04 +0400

    salam

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -15,7 +15,7 @@ export default function Footer() {
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
           <a href="https://github.com/saadpasta/developerFolio">
-            developerFolio
+            {/* developerFolio */}
           </a>
         </p>
       </div>
