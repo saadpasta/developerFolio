d5e139fa6e644db25eab37e33ea95fb63e4ae69f
Michael Marsland <mmarsland@mac.com>
2023-04-08 15:14:36 -0700

    Fix Variable Name for Error Throwing
    Fixes a typo issue when throwing an error based on the Medium Username

--- fetch.js
+++ fetch.js
@@ -108,7 +108,7 @@ if (MEDIUM_USERNAME !== undefined) {
 
     console.log(`statusCode: ${res.statusCode}`);
     if (res.statusCode !== 200) {
-      throw new Error(ERR.requestMediumFailed);
+      throw new Error(ERR.requestFailedMedium);
     }
 
     res.on("data", d => {
