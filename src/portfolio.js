935866aca067ea228d499cd2c9033c11bd7198be
Roman0nyshchuk <126744790+Roman0nyshchuk@users.noreply.github.com>
2023-12-09 11:42:24 -0500

    name

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,7 +21,7 @@ const illustration = {
 
 const greeting = {
   username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  title: "Hi all, I'm Roman",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
