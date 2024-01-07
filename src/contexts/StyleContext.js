bb737b02b0efe69d90ef4c0fef40f237b956a9d1
Dereje Getahun <getahun.d@gmail.com>
2024-01-07 14:20:32 +0300

    Updates

--- src/contexts/StyleContext.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const StyleContext = React.createContext();
-
-export const StyleProvider = StyleContext.Provider;
-export const StyleConsumer = StyleContext.Consumer;
-
-export default StyleContext;
