fc545373a8e0e95a19d2635065f6554a6482b808
hassanmehdi <hassanmehdi510@gmail.com>
2024-02-20 23:41:34 +0500

    Updated quick links

--- src/portfolio.js
+++ src/portfolio.js
@@ -33,13 +33,15 @@ const greeting = {
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  github: "https://github.com/DevHassanMehdi",
+  linkedin: "https://www.linkedin.com/in/devhassanmehdi/",
+  gmail: "hassanmehdi510@gmail.com",
+  twitter: "https://twitter.com/DevHassanMehdi",
+  facebook: "https://www.facebook.com/devhassanmehdi/",
+  instagram: "https://www.instagram.com/devhassanmehdi/",
+  // kaggle: "https://gitlab.com/saadpasta",
+  // medium: "https://medium.com/@saadpasta",
+  // stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
