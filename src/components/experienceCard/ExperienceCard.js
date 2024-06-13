a975f49e30e66c936672dfa72ea70e01e9937491
Ubed Khan <61332446+ubednama@users.noreply.github.com>
2024-06-13 18:25:58 +0000

    feat: removed logo icon from experince card if no company logo is provided

--- src/components/experienceCard/ExperienceCard.js
+++ src/components/experienceCard/ExperienceCard.js
@@ -38,14 +38,14 @@ export default function ExperienceCard({cardInfo, isDark}) {
           <h5 className="experience-text-company">{cardInfo.company}</h5>
         </div>
 
-        <img
+        {cardInfo.companylogo && (<img
           crossOrigin={"anonymous"}
           ref={imgRef}
           className="experience-roundedimg"
           src={cardInfo.companylogo}
           alt={cardInfo.company}
           onLoad={() => getColorArrays()}
-        />
+        />)}
       </div>
       <div className="experience-text-details">
         <h5
