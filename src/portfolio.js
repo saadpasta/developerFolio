9b8962420cb448cca8c1db889ed0097ff1d5c268
Lucas <lqiu03@ucla.edu>
2024-08-20 17:29:31 -0700

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,26 +20,22 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Lucas Qiu",
+  title: "Hi all, I'm Lucas Qiu",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "A passionate Data Engineer & Architect 🚀 with experience in Business Intelligence & Sports Analytics."
   ),
-  resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
-  displayGreeting: true // Set false to hide this section, defaults to true
+
 };
 
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  github: "https://github.com/lqiu03",
+  linkedin: "https://www.linkedin.com/in/lucas-qiu/",
+  gmail: "lqiu03@ucla.edu",
+  medium: "https://medium.com/@lqiu03",
+ tableau: "https://public.tableau.com/app/profile/lucas.qiu/vizzes"
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
@@ -48,8 +44,8 @@ const socialMediaLinks = {
 // Skills Section
 
 const skillsSection = {
-  title: "What I do",
-  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  title: "MY PROFICIENCIES",
+  subTitle: "SELF-MOTIVED PROBLEM SOLVER WHO WANTS TO EXPLORE TECH STACK",
   skills: [
     emoji(
       "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
@@ -63,60 +59,69 @@ const skillsSection = {
   /* Make Sure to include correct Font Awesome Classname to view your icon
 https://fontawesome.com/icons?d=gallery */
 
-  softwareSkills: [
-    {
-      skillName: "html-5",
-      fontAwesomeClassname: "fab fa-html5"
-    },
-    {
-      skillName: "css3",
-      fontAwesomeClassname: "fab fa-css3-alt"
-    },
-    {
-      skillName: "sass",
-      fontAwesomeClassname: "fab fa-sass"
-    },
-    {
-      skillName: "JavaScript",
-      fontAwesomeClassname: "fab fa-js"
-    },
-    {
-      skillName: "reactjs",
-      fontAwesomeClassname: "fab fa-react"
-    },
-    {
-      skillName: "nodejs",
-      fontAwesomeClassname: "fab fa-node"
-    },
-    {
-      skillName: "swift",
-      fontAwesomeClassname: "fab fa-swift"
-    },
-    {
-      skillName: "npm",
-      fontAwesomeClassname: "fab fa-npm"
-    },
-    {
-      skillName: "sql-database",
-      fontAwesomeClassname: "fas fa-database"
-    },
-    {
-      skillName: "aws",
-      fontAwesomeClassname: "fab fa-aws"
-    },
-    {
-      skillName: "firebase",
-      fontAwesomeClassname: "fas fa-fire"
-    },
-    {
-      skillName: "python",
-      fontAwesomeClassname: "fab fa-python"
-    },
-    {
-      skillName: "docker",
-      fontAwesomeClassname: "fab fa-docker"
-    }
-  ],
+softwareSkills: {
+    coding: [
+      {
+        skillName: "C++",
+        fontAwesomeClassname: "fas fa-code"
+      },
+      {
+        skillName: "SQL",
+        fontAwesomeClassname: "fas fa-database"
+      },
+      {
+        skillName: "R",
+        fontAwesomeClassname: "fab fa-r-project"
+      },
+      {
+        skillName: "Python",
+        fontAwesomeClassname: "fab fa-python"
+      }
+    ],
+    dataStorage: [
+      {
+        skillName: "Databricks (Azure)",
+        fontAwesomeClassname: "fab fa-microsoft"
+      },
+      {
+        skillName: "Databricks (AWS)",
+        fontAwesomeClassname: "fab fa-aws"
+      },
+      {
+        skillName: "Snowflake",
+        fontAwesomeClassname: "fas fa-snowflake"
+      },
+      {
+        skillName: "OracleSQL",
+        fontAwesomeClassname: "fas fa-database"
+      },
+      {
+        skillName: "PostgreSQL",
+        fontAwesomeClassname: "fas fa-database"
+      }
+    ],
+    dataPipeline: [
+      {
+        skillName: "Apache Kafka",
+        fontAwesomeClassname: "fas fa-stream"
+      }
+    ],
+    visualization: [
+      {
+        skillName: "Tableau",
+        fontAwesomeClassname: "fas fa-chart-bar"
+      },
+      {
+        skillName: "Power BI",
+        fontAwesomeClassname: "fas fa-chart-pie"
+      },
+      {
+        skillName: "Google Colab",
+        fontAwesomeClassname: "fas fa-laptop-code"
+      }
+    ]
+  }
+  
   display: true // Set false to hide this section, defaults to true
 };
 
@@ -126,47 +131,20 @@ const educationInfo = {
   display: true, // Set false to hide this section, defaults to true
   schools: [
     {
-      schoolName: "Harvard University",
-      logo: require("./assets/images/harvardLogo.png"),
-      subHeader: "Master of Science in Computer Science",
-      duration: "September 2017 - April 2019",
-      desc: "Participated in the research of XXX and published 3 papers.",
+      schoolName: "UCLA",
+      logo: require("/Users/lucasqiu/Desktop/uclalogo.png"),
+      subHeader: "B.S. Biology Honors - B.A. History, Double Major",
+      duration: "September 2021 - June 2025",
+      desc: "Specialization in Computing, Physics 5A (Mechanics & Energy) Course Reader, UCLA Undergraudate Student Government, UCLA Athletics, CSSA-UCLA, TEDxUCLA, Aleph UCLA Undergraduate Journal",
       descBullets: [
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
       ]
     },
-    {
-      schoolName: "Stanford University",
-      logo: require("./assets/images/stanfordLogo.png"),
-      subHeader: "Bachelor of Science in Computer Science",
-      duration: "September 2013 - April 2017",
-      desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
-      descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
-    }
   ]
 };
 
-// Your top 3 proficient stacks/tech experience
 
-const techStack = {
-  viewSkillBars: true, //Set it to true to show Proficiency Section
-  experience: [
-    {
-      Stack: "Frontend/Design", //Insert stack or technology you have experience in
-      progressPercentage: "90%" //Insert relative proficiency in percentage
-    },
-    {
-      Stack: "Backend",
-      progressPercentage: "70%"
-    },
-    {
-      Stack: "Programming",
-      progressPercentage: "60%"
-    }
-  ],
-  displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
-};
 
 // Work experience section
 
@@ -174,30 +152,37 @@ const workExperiences = {
   display: true, //Set it to true to show workExperiences Section
   experience: [
     {
-      role: "Software Engineer",
-      company: "Facebook",
-      companylogo: require("./assets/images/facebookLogo.png"),
-      date: "June 2018 – Present",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      role: "Data Engineer, UCLA Tennis (BIG TEN)",
+      company: "UCLA Athletics",
+      companylogo: require("/Users/lucasqiu/Desktop/logo/athleticslogo.png"),
+      date: "June 2024 – Present",
+      desc: "Tennis player performance analytics, data cleaning, query optimization, BI visualization",
       descBullets: [
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
       ]
     },
     {
-      role: "Front-End Developer",
-      company: "Quora",
-      companylogo: require("./assets/images/quoraLogo.png"),
-      date: "May 2017 – May 2018",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      role: "Intern, Rose Bowl Legacy",
+      company: "Rose Bowl Stadium",
+      companylogo: require("/Users/lucasqiu/Desktop/logo/rosebowl.png"),
+      date: "June 2024 – Present",
+      desc: "2025 Rose Bowl Game (College Football Playoff), UCLA Football, Rose Bowl Concerts & Foundation"
     },
     {
-      role: "Software Engineer Intern",
-      company: "Airbnb",
-      companylogo: require("./assets/images/airbnbLogo.png"),
-      date: "Jan 2015 – Sep 2015",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      role: "Football Data Architect",
+      company: "AMSG FC",
+      companylogo: require("/Users/lucasqiu/Desktop/logo/amsg.png"),
+      date: "May 2024 – Aug 2024",
+      desc: "Player performance analytics, Data warehouse management, Stadium technology"
     }
+    {
+        role: "Team Manager, UCLA Women's Volleyball (PAC 12)",
+        company: "UCLA Athletics",
+        companylogo: require("/Users/lucasqiu/Desktop/logo/uclalogo.png"),
+        date: "June 2023 – June 2024",
+        desc: "Game-day & scrimmage team operations"
+      }
   ]
 };
 
@@ -212,176 +197,67 @@ const openSource = {
 // Some big projects you have worked on
 
 const bigProjects = {
-  title: "Big Projects",
-  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  title: "Projects",
+  subtitle: "Repository of my coding portfolio",
   projects: [
     {
-      image: require("./assets/images/saayaHealthLogo.webp"),
-      projectName: "Saayahealth",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      image: require("/Users/lucasqiu/Desktop/logo/paris.png"),
+      projectName: "Analysis of Performance Metrics in Women's Singles Gold Medal Match",
+      projectDesc: "Zheng Q. vs. Vekic D. | Paris Olympics 2024",
       footerLink: [
         {
-          name: "Visit Website",
-          url: "http://saayahealth.com/"
+          name: "Visit Tableau",
+          url: "https://public.tableau.com/app/profile/lucas.qiu/viz/AnalysisofCriticalShotsandPerformanceMetricsinWomensSinglesGoldMedalMatchParisOlympics2024/Dashboard2/"
         }
         //  you can add extra buttons here.
       ]
     },
     {
-      image: require("./assets/images/nextuLogo.webp"),
-      projectName: "Nextu",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      image: require("/Users/lucasqiu/Desktop/logo/rams.png"),
+      projectName: "LA Rams: Offensive and Defensive Performance",
+      projectDesc: "Analyzing NFL Play-by-Play data in R Studio",
       footerLink: [
         {
-          name: "Visit Website",
-          url: "http://nextu.se/"
+          name: "Visit Medium",
+          url: "https://medium.com/@lqiu03/analyzing-the-la-rams-2023-nfl-season-with-r-studio-offensive-and-defensive-performance-metrics-2c5b65335064/"
         }
       ]
     }
+    {
+        image: require("/Users/lucasqiu/Desktop/logo/euro.png"),
+        projectName: "UEFA EURO 24 - Finals Interactive Starting Line-ups",
+        projectDesc: "England vs. Spain @Olympiastadion, Berlin",
+        footerLink: [
+          {
+            name: "Visit Tableau",
+            url: "https://public.tableau.com/app/profile/lucas.qiu/viz/UEFAEURO2024-FinalsStartingLine-upInteractivePreview/Dashboard1"
+          }
+        ]
+      }
+    {
+        image: require("/Users/lucasqiu/Desktop/logo/fcb.png"),
+        projectName: "Lamine Yamal (FC Barcelona)'s Performance Analytics",
+        projectDesc: "Football performance analytics with Python Jupyter",
+        footerLink: [
+          {
+            name: "Visit Medium",
+            url: "https://medium.com/@lqiu03/hes-only-16-a-tableau-r-and-python-showcase-of-lamine-yamal-e16734988f3f"
+          }
+        ]
+      }
   ],
   display: true // Set false to hide this section, defaults to true
 };
 
-// Achievement Section
-// Include certificates, talks etc
 
-const achievementSection = {
-  title: emoji("Achievements And Certifications 🏆 "),
-  subtitle:
-    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
-
-  achievementsCards: [
-    {
-      title: "Google Code-In Finalist",
-      subtitle:
-        "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
-      image: require("./assets/images/codeInLogo.webp"),
-      imageAlt: "Google Code-In Logo",
-      footerLink: [
-        {
-          name: "Certification",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing"
-        },
-        {
-          name: "Award Letter",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
-        },
-        {
-          name: "Google Code-in Blog",
-          url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
-        }
-      ]
-    },
-    {
-      title: "Google Assistant Action",
-      subtitle:
-        "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
-      image: require("./assets/images/googleAssistantLogo.webp"),
-      imageAlt: "Google Assistant Action Logo",
-      footerLink: [
-        {
-          name: "View Google Assistant Action",
-          url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en"
-        }
-      ]
-    },
-
-    {
-      title: "PWA Web App Developer",
-      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
-      image: require("./assets/images/pwaLogo.webp"),
-      imageAlt: "PWA Logo",
-      footerLink: [
-        {name: "Certification", url: ""},
-        {
-          name: "Final Project",
-          url: "https://pakistan-olx-1.firebaseapp.com/"
-        }
-      ]
-    }
-  ],
-  display: true // Set false to hide this section, defaults to true
-};
-
-// Blogs Section
-
-const blogSection = {
-  title: "Blogs",
-  subtitle:
-    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
-  displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
-  blogs: [
-    {
-      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
-      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
-      description:
-        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
-    },
-    {
-      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
-      title: "Why REACT is The Best?",
-      description:
-        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
-    }
-  ],
-  display: true // Set false to hide this section, defaults to true
-};
-
-// Talks Sections
-
-const talkSection = {
-  title: "TALKS",
-  subtitle: emoji(
-    "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"
-  ),
-
-  talks: [
-    {
-      title: "Build Actions For Google Assistant",
-      subtitle: "Codelab at GDG DevFest Karachi 2019",
-      slides_url: "https://bit.ly/saadpasta-slides",
-      event_url: "https://www.facebook.com/events/2339906106275053/"
-    }
-  ],
-  display: true // Set false to hide this section, defaults to true
-};
-
-// Podcast Section
-
-const podcastSection = {
-  title: emoji("Podcast 🎙️"),
-  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
-
-  // Please Provide with Your Podcast embeded Link
-  podcast: [
-    "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
-  ],
-  display: true // Set false to hide this section, defaults to true
-};
-
-// Resume Section
-const resumeSection = {
-  title: "Resume",
-  subtitle: "Feel free to download my resume",
-
-  // Please Provide with Your Podcast embeded Link
-  display: true // Set false to hide this section, defaults to true
-};
 
 const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
-  email_address: "saadpasta70@gmail.com"
+  email_address: "lqiu03@ucla.com"
 };
 
-// Twitter Section
-
-const twitterDetails = {
-  userName: "twitter", //Replace "twitter" with your twitter username without @
-  display: true // Set true to display this section, defaults to false
-};
 
 const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
 
