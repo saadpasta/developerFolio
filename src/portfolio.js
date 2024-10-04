0afacd1c964d9322ff88e9ababcf7d4d1fbe8d05
Saad Rasheed <84860156+saadrasheeddev@users.noreply.github.com>
2024-10-04 15:47:15 +0500

    No phone number

--- src/portfolio.js
+++ src/portfolio.js
@@ -373,7 +373,7 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-3322468679",
+  // number: "+92-3322468679",
   email_address: "saadrasheeddev@gmail.com"
 };
 
