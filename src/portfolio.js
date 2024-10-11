71950048141986c977e315ceb35c492daeb4c7bc
Saad Rasheed <84860156+saadrasheeddev@users.noreply.github.com>
2024-10-11 10:33:19 +0500

    Edited Open to Opportunities

--- src/portfolio.js
+++ src/portfolio.js
@@ -390,7 +390,7 @@ const twitterDetails = {
   display: true // Set true to display this section, defaults to false
 };
 
-const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
+const isHireable = true; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
 
 export {
   illustration,
