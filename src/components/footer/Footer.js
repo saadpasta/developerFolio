f4bc0bc46305c23f0e9315546cea917d6d91ab77
Gokul Raja <106546785+Gokul-Raja84@users.noreply.github.com>
2024-10-18 22:12:53 +0530

    Added target='_blank' and rel='noreferrer' to external link in Footer… (#719)
    * Added target='_blank' and rel='noreferrer' to external link in Footer component
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -14,7 +14,11 @@ export default function Footer() {
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
-          <a href="https://github.com/saadpasta/developerFolio">
+          <a
+            href="https://github.com/saadpasta/developerFolio"
+            target="_blank"
+            rel="noreferrer"
+          >
             developerFolio
           </a>
         </p>
