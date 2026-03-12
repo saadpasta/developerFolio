b6a1e8510c24ad1b6f3ec779065bfafd5e28f222
khushi2005 <khushi.kp28@gmial.com>
2026-03-12 20:47:53 +0530

    Dark Mode Background color

--- src/components/ToggleSwitch/ToggleSwitch.js
+++ src/components/ToggleSwitch/ToggleSwitch.js
@@ -3,6 +3,7 @@ import emoji from "react-easy-emoji";
 import StyleContext from "../../contexts/StyleContext";
 import "./ToggleSwitch.scss";
 
+
 const ToggleSwitch = () => {
   const {isDark} = useContext(StyleContext);
   const [isChecked, setChecked] = useState(isDark);
